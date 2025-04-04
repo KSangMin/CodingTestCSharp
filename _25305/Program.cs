3d5b7c2e2139bda31c6c9ae46f39859f9ad65815
KSangMin <fhfpszmfnwm@naver.com>
2025-04-04 21:17:38 +0900

    [25305] 커트라인

---
+++ _25305/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] input = Console.ReadLine().Split();
+int n = int.Parse(input[0]);
+int k = int.Parse(input[1]);
+
+string[] splits = Console.ReadLine().Split();
+int[] scores = new int[n];
+
+for(int i = 0; i < n; i++)
+{
+    scores[i] = int.Parse(splits[i]);
+}
+
+Array.Sort(scores);
+
+Console.WriteLine(scores[n - (k)]);
