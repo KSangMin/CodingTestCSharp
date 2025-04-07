bec8a7075a271f2388035fe4dab95159d9be56db
KSangMin <fhfpszmfnwm@naver.com>
2025-04-07 21:09:50 +0900

    [17388] 와글와글 숭고한

---
+++ _17388/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _17388
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int[] score = new int[input.Length];
+            
+            for(int i = 0; i < input.Length; i++)
+            {
+                score[i] = int.Parse(input[i]);
+            }
+
+            if (score.Sum() >= 100) Console.WriteLine("OK");
+            else
+            {
+                int min = score.Min();
+
+                if (min == score[0]) Console.WriteLine("Soongsil");
+                else if (min == score[1]) Console.WriteLine("Korea");
+                else Console.WriteLine("Hanyang");
+            }
+        }
+    }
+}
