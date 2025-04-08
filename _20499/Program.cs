5f38d73be7d2c68a9ffbf8679f41b0270791e3d5
KSangMin <fhfpszmfnwm@naver.com>
2025-04-08 21:03:02 +0900

    [20499] Darius님 한타 안 함?

---
+++ _20499/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _20499
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("/");
+            int[] kda = input.Select(int.Parse).ToArray();
+
+            if (kda[0] + kda[2] < kda[1] || kda[1] == 0) Console.WriteLine("hasu");
+            else Console.WriteLine("gosu");
+        }
+    }
+}
