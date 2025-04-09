6b798b114bdcdbdc4f5a7f9940dee3632df2fb56
KSangMin <fhfpszmfnwm@naver.com>
2025-04-09 20:55:54 +0900

    [15726] 이칙연산

---
+++ _15726/Program.cs
@@ -0,0 +1,7 @@
+﻿double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+double ans = nums[0];
+if (nums[1] < nums[2]) ans = ans / nums[1] * nums[2];
+else ans = ans * nums[1] / nums[2];
+
+Console.WriteLine((int)ans);
