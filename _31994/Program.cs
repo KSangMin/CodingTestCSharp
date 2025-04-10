fa16850bd85512e0c0331b74c21c88fc61b7dcfc
KSangMin <fhfpszmfnwm@naver.com>
2025-04-10 20:38:33 +0900

    [31994] 강당 대관

---
+++ _31994/Program.cs
@@ -0,0 +1,12 @@
+﻿Dictionary<string, int> seminar = new();
+
+for(int i = 0; i < 7; i++)
+{
+    string[] input = Console.ReadLine().Split();
+
+    seminar[input[0]] = int.Parse(input[1]);
+}
+
+string ans = seminar.MaxBy(d => d.Value).Key;
+
+Console.WriteLine(ans);
