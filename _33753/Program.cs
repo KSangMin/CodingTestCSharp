b03f590abe7763d004a9ceba3971a2bcf0904eb4
KSangMin <fhfpszmfnwm@naver.com>
2025-04-11 22:20:39 +0900

    [33753] 주차 요금 정산하기

---
+++ _33753/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int t = int.Parse(Console.ReadLine());
+
+int sum = nums[0];
+t -= 30;
+if (t > 0) { sum += t / nums[1] * nums[2]; t %= nums[1]; }
+if (t > 0) sum += nums[2]; 
+
+Console.WriteLine(sum);
