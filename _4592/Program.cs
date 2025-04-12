bfc7fbefc95987c12564d33d0eb09faa3fdf98a9
KSangMin <fhfpszmfnwm@naver.com>
2025-04-13 00:35:43 +0900

    [4592] 중복을 없애자

---
+++ _4592/Program.cs
@@ -0,0 +1,16 @@
+﻿while (true)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    if (input[0] == 0) break;
+
+    List<int> ans = new() { input[1] };
+
+    for (int i = 2; i <= input[0]; i++)
+    {
+        if (input[i - 1] != input[i]) ans.Add(input[i]);
+    }
+
+    foreach (int i in ans) Console.Write(i + " ");
+    Console.WriteLine("$");
+}
