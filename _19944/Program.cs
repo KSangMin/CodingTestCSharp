224d1dbe6c5820de0062424f7e09309b56495962
KSangMin <fhfpszmfnwm@naver.com>
2025-04-13 23:53:09 +0900

    [19944] 뉴비의 기준은 뭘까?

---
+++ _19944/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int n = input[0];
+int m = input[1];
+
+string ans;
+if (m <= 2) ans = "NEWBIE!";
+else if (m <= n) ans = "OLDBIE!";
+else ans = "TLE!";
+
+Console.WriteLine(ans);
