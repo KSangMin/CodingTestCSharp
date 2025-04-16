d3ff9d917ac0c1088ff29dd61d3b08b4595cbde0
KSangMin <fhfpszmfnwm@naver.com>
2025-04-16 21:08:03 +0900

    [27310] :chino_shock:

---
+++ _27310/Program.cs
@@ -0,0 +1,19 @@
+﻿string input = Console.ReadLine();
+
+int sum = input.Length;
+foreach (char c in input)
+{
+    switch (c)
+    {
+        case ':':
+            sum++;
+            break;
+        case '_':
+            sum += 5;
+            break;
+        default:
+            break;
+    }
+}
+
+Console.WriteLine(sum);
