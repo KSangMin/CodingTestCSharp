86ef212c4a2a40eb0ec37f18e9a267eb8d0ec80c
KSangMin <fhfpszmfnwm@naver.com>
2025-04-21 20:38:48 +0900

    [25191] 치킨댄스를 추는 곰곰이를 본 임스

---
+++ _25191/Program.cs
@@ -0,0 +1,4 @@
+﻿int chicken = int.Parse(Console.ReadLine());
+int[] drinks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Console.WriteLine(MathF.Min(chicken, drinks[0] / 2 + drinks[1]));
