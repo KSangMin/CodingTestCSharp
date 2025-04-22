dbbd77c9523843fabfb704347155da3a0714f423
KSangMin <fhfpszmfnwm@naver.com>
2025-04-22 21:01:54 +0900

    [4562] No Brainer

---
+++ _4562/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    int[] brains = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    if (brains[0] >= brains[1]) Console.WriteLine("MMM BRAINS");
+    else Console.WriteLine("NO BRAINS");
+}
