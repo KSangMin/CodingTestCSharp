d263753685aa5f54a8764672ef796d7f8529836e
KSangMin <fhfpszmfnwm@naver.com>
2025-04-23 20:54:17 +0900

    [19698] 헛간 청약

---
+++ _19698/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int n = input[0], w = input[1], h  = input[2], l  = input[3];
+
+Console.WriteLine(MathF.Min((w / l) * (h / l), n));
