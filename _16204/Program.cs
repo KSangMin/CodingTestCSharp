49b6b66d6b2ff624638cedba1b482775a9153373
KSangMin <fhfpszmfnwm@naver.com>
2025-04-25 21:13:52 +0900

    [16204] 카드 뽑기

---
+++ _16204/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int baseO = input[1], baseX = input[0] - input[1];
+int newO = input[2], newX = input[0] - input[2];
+
+int sum = (int)(MathF.Min(baseO, newO) + MathF.Min(baseX, newX));
+
+Console.WriteLine(sum);
