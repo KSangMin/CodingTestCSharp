26da51a0d0ae8318614a6376c964d2875caf22fb
KSangMin <fhfpszmfnwm@naver.com>
2025-04-27 23:52:17 +0900

    [10178] 할로윈의 사탕

---
+++ _10178/Program.cs
@@ -0,0 +1,12 @@
+﻿int test = int.Parse(Console.ReadLine());
+
+for(int i = 0; i< test; i++)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    int c = input[0], v = input[1];
+
+    int child = c / v, dad = c % v;
+
+    Console.WriteLine($"You get {child} piece(s) and your dad gets {dad} piece(s).");
+}
