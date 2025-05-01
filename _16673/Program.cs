d46df7604d0293a217265115b25f1954bc28587b
KSangMin <fhfpszmfnwm@naver.com>
2025-05-01 20:45:48 +0900

    [16673] 고려대학교에는 공식 와인이 있다

---
+++ _16673/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] nkp = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int n = nkp[0], k = nkp[1], p  = nkp[2];
+
+int sum = 0;
+for(int i = 1;  i <= n; i++)
+{
+    sum += (k + p * i) * i;
+}
+
+Console.WriteLine(sum);
