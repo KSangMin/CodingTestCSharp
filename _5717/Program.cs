5d0f350083ee095129e91ebe7dcb6eae258f085c
KSangMin <fhfpszmfnwm@naver.com>
2025-05-06 21:16:34 +0900

    [5717] 상근이의 친구들

---
+++ _5717/Program.cs
@@ -0,0 +1,8 @@
+﻿while (true)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    if (input[0] == 0 && input[1] == 0) return;
+
+    Console.WriteLine(input[0] + input[1]);
+}
