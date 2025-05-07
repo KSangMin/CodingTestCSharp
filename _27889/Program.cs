7227101668c1a6803d38c2054e4185e7b37311a6
KSangMin <fhfpszmfnwm@naver.com>
2025-05-07 21:19:32 +0900

    [27889] 특별한 학교 이름

---
+++ _27889/Program.cs
@@ -0,0 +1,11 @@
+﻿string code = Console.ReadLine();
+
+Dictionary<string, string> names = new()
+{
+    {"NLCS",  "North London Collegiate School"},
+    {"BHA", "Branksome Hall Asia"},
+    {"KIS", "Korea International School"},
+    {"SJA", "St. Johnsbury Academy"}
+};
+
+Console.WriteLine(names[code]);
