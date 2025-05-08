182eae26c6524c128068daa705a10d07d7498b7d
KSangMin <fhfpszmfnwm@naver.com>
2025-05-08 21:23:04 +0900

    [13129] Disposable Cup

---
+++ _13129/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int a = input[0], b = input[1], n = input[2];
+
+int sum = a * n;
+for(int i = 0; i < n; i++)
+{
+    sum += b;
+    Console.WriteLine(sum);
+}
