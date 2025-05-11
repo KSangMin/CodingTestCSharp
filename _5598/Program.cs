2118708e67b7a713dc77339f69577cc724d4b12d
KSangMin <fhfpszmfnwm@naver.com>
2025-05-11 22:34:31 +0900

    [5598] 카이사르 암호

---
+++ _5598/Program.cs
@@ -0,0 +1,12 @@
+﻿string input = Console.ReadLine();
+
+string ans = "";
+
+foreach(char c in input)
+{
+    char target = (char)(c - 3);
+    if (target < 'A') ans += (char)(target + 26);
+    else ans += target;
+}
+
+Console.WriteLine(ans);
