96149a35ad0f829316e8ee3df67180e4e30745f2
KSangMin <fhfpszmfnwm@naver.com>
2025-05-12 21:19:58 +0900

    [14219] 막대과자 포장

---
+++ _14219/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int n = nm[0], m  = nm[1];
+if ((n >= 3 || m >= 3) && (n % 3 == 0 || m % 3 == 0))
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
