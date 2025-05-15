9016f400fbc83d57fb355fd380fe3e304d67a4c8
KSangMin <fhfpszmfnwm@naver.com>
2025-05-15 21:25:52 +0900

    [11134] 쿠키애호가

---
+++ _11134/Program.cs
@@ -0,0 +1,10 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < t; i++)
+{
+    int[] cookie = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    int n  = cookie[0], c = cookie[1];
+
+    Console.WriteLine((n / c) + ((n % c == 0) ? 0 : 1));
+}
