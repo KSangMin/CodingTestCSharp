86c3bc59fa7abe3953983d846b94a40323dbd7e8
KSangMin <fhfpszmfnwm@naver.com>
2025-05-19 21:46:34 +0900

    [13163] 닉네임에 갓 붙이기

---
+++ _13163/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    string[] name = Console.ReadLine().Split();
+
+    string ans = "god" + string.Join("", name.Skip(1));
+
+    Console.WriteLine(ans);
+}
