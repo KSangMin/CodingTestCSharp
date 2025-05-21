93a969cd2ffc2f456ca55f6670d663c284c8ab66
KSangMin <fhfpszmfnwm@naver.com>
2025-05-21 21:08:27 +0900

    [Feat] 파일 옮기기

---
+++ _11943/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] first = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] second = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Console.WriteLine(MathF.Min(first[0] + second[1], second[0] + first[1]));
