981885a838bde4a1f42cf49642fc9a1c9b2c557c
KSangMin <fhfpszmfnwm@naver.com>
2025-05-26 01:19:42 +0900

    [28074] 모비스

---
+++ _28074/Program.cs
@@ -0,0 +1,12 @@
+﻿string mobis = Console.ReadLine();
+
+bool isMobis =
+    (
+        mobis.Contains('M')
+        && mobis.Contains('O')
+        && mobis.Contains('B')
+        && mobis.Contains('I')
+        && mobis.Contains('S')
+    );
+
+Console.WriteLine(isMobis ? "YES" : "NO");
