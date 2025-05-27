d81ad0881f4595e1d044485bbedd985d19556daa
KSangMin <fhfpszmfnwm@naver.com>
2025-05-27 22:35:59 +0900

    [11648] 지속

---
+++ _11648/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int cnt = 0;
+while(n > 9)
+{
+    cnt++;
+
+    int sum = 1;
+    while(n > 0)
+    {
+        sum *= n % 10;
+        n /= 10;
+    }
+
+    n = sum;
+}
+
+Console.WriteLine(cnt);
