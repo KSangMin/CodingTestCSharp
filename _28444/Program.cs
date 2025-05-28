677edd1f5a8335e55ab1759fafaa8042672d2e1f
KSangMin <fhfpszmfnwm@naver.com>
2025-05-28 21:41:32 +0900

    [28444] HI-ARC=?

---
+++ _28444/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] hiarc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int h = hiarc[0], i = hiarc[1], a  = hiarc[2], r  = hiarc[3], c  = hiarc[4];
+
+Console.WriteLine((h * i) - (a * r * c));
