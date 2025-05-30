57092488ddacc7b597762867d9b38121af1ae0d9
KSangMin <fhfpszmfnwm@naver.com>
2025-05-31 00:06:03 +0900

    [10102] 개표

---
+++ _10102/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string vote = Console.ReadLine();
+
+int[] cnt = { 0, 0 };
+foreach(char c in vote)
+{
+    if(c == 'A') cnt[0]++;
+    else cnt[1]++;
+}
+
+if (cnt[0] > cnt[1]) Console.WriteLine("A");
+else if (cnt[0] < cnt[1]) Console.WriteLine("B");
+else Console.WriteLine("Tie");
