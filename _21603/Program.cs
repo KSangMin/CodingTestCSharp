e4ecb2e32450fd30dd03cc7e4dc9d0b14738f0ff
KSangMin <fhfpszmfnwm@naver.com>
2025-06-01 00:51:59 +0900

    [21603] K 2K 게임

---
+++ _21603/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] nk = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int n = nk[0], k = nk[1];
+int firstK = k % 10;
+int first2K = (2 * k) % 10;
+List<int> ans = new();
+
+for (int i = 1; i <= n; i++)
+{
+    int first = i % 10;
+    if (first != firstK && first != first2K) ans.Add(i);
+}
+
+Console.WriteLine(ans.Count);
+for(int i = 0; i < ans.Count; i++)
+{
+    Console.Write($"{ans[i]} ");
+}
