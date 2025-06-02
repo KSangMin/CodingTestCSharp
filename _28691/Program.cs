2acd17f6a3a0c619ee9132e4b1fcfa9c75217277
KSangMin <fhfpszmfnwm@naver.com>
2025-06-02 23:10:55 +0900

    [28691] 정보보호학부 동아리 소개

---
+++ _28691/Program.cs
@@ -0,0 +1,12 @@
+﻿Dictionary<char, string> names = new()
+    { 
+        { 'M', "MatKor" }
+        , { 'W', "WiCys" }
+        , { 'C', "CyKor" }
+        , { 'A', "AlKor" }
+        , { '$', "$clear" }
+    };
+
+char c = char.Parse(Console.ReadLine());
+
+Console.WriteLine(names[c]);
