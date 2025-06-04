7a47422e1814310b0f89721dc41e74686aeea3bf
KSangMin <fhfpszmfnwm@naver.com>
2025-06-04 13:25:48 +0900

    [2193] 이친수

---
+++ _2193/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(FindFib(n));
+
+long FindFib(int n)
+{
+    List<long> fibs = new() { 0, 1, 1 };
+
+    for(int i = 3; i <= n; i++)
+    {
+        fibs.Add(fibs[i - 1] + fibs[i - 2]);
+    }
+
+    return fibs[n];
+}
