5abd10a463337f4e19cb7c82c388d8375ce5be16
KSangMin <fhfpszmfnwm@naver.com>
2025-06-05 17:52:45 +0900

    [1912] 연속합

---
+++ _1912/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int[] dp = new int[nums.Length];
+
+dp[0] = nums[0];
+for (int i = 1; i < nums.Length; i++)
+{
+    dp[i] = (int)MathF.Max(nums[i], dp[i - 1] + nums[i]);
+}
+
+Console.WriteLine(dp.Max());
