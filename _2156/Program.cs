0e759b14733f6d098a8fa5b3f6054511f40350e7
KSangMin <fhfpszmfnwm@naver.com>
2025-06-06 11:57:55 +0900

    [2156] 포도주 시식

---
+++ _2156/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] wines = new int[n + 1], dp = new int[n + 1];
+for(int i = 1; i <= n; i++)
+{
+    wines[i] = int.Parse(Console.ReadLine());
+}
+
+dp[1] = wines[1];
+if(n >= 2)dp[2] = wines[1] + wines[2];
+for (int i = 3; i <= n; i++)
+{
+    dp[i] = (int)MathF.Max(
+        MathF.Max(dp[i - 1], wines[i] + dp[i - 2]),
+        wines[i] + wines[i - 1] + dp[i - 3]
+    );
+}
+
+Console.WriteLine(dp[n]);
