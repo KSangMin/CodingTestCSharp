e718374db198d720eef5bf921b59bf5bbe2ab343
KSangMin <fhfpszmfnwm@naver.com>
2025-06-07 15:50:29 +0900

    [14501] 퇴사

--- _14501/Program.cs
+++ _14501/Program.cs
@@ -1,3 +1,23 @@
-﻿string[] input = Console.ReadLine().Split().ToArray();
+﻿int n = int.Parse(Console.ReadLine());
 
-Console.WriteLine(input[0] == input[1] ? 1 : 0);
+int[] times = new int[n + 1], prices = new int[n + 1], dp = new int[n + 1];
+
+for (int i = 1; i <= n; i++)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    times[i] = input[0];
+    prices[i] = input[1];
+}
+
+for(int i = 1; i <= n; i++)
+{
+    int target = i + times[i] - 1;
+    if (target <= n)
+    {
+        int sum = dp[i - 1] + prices[i];
+        dp[target] = Math.Max(dp[target], sum);
+    }
+    dp[i] = (int)MathF.Max(dp[i], dp[i - 1]);
+}
+
+Console.WriteLine(dp[n]);
