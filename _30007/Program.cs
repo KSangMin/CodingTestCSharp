0137d2cab0f6a441313d7d583973a20fa22f5800
KSangMin <fhfpszmfnwm@naver.com>
2025-06-08 22:48:50 +0900

    [30007] 라면 공식

---
+++ _30007/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    int A = input[0], B = input[1], X = input[2];
+
+    Console.WriteLine((A * (X - 1)) + B);
+}
