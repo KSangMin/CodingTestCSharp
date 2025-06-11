a159bb5040e981ff160a5e0dce6b427f3eb04849
KSangMin <fhfpszmfnwm@naver.com>
2025-06-12 00:46:15 +0900

    [7510] 고급 수학

---
+++ _7510/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= n; i++)
+{
+    int[] abc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    Array.Sort(abc);
+    int a = abc[0], b = abc[1], c = abc[2];
+
+    Console.WriteLine($"Scenario #{i}:");
+    Console.WriteLine(a * a + b * b == c * c ? "yes\n" : "no\n");
+}
