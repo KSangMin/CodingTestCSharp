7d66c1b05998787bb64d9417ec691dcc896ea91a
KSangMin <fhfpszmfnwm@naver.com>
2025-06-13 01:19:23 +0900

    [1547] 공

---
+++ _1547/Program.cs
@@ -0,0 +1,12 @@
+﻿int m = int.Parse(Console.ReadLine());
+
+int cur = 1;
+for(int i = 1; i <= m; i++)
+{
+    int[] ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    int a = ab[0], b = ab[1];
+    if (a == cur) cur = b;
+    else if (b == cur) cur = a;
+}
+
+Console.WriteLine(cur);
