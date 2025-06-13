c3542bb4110b20fbc6a251687dab3f781095c8b1
KSangMin <fhfpszmfnwm@naver.com>
2025-06-13 16:44:14 +0900

    [15722] 빙글빙글 스네일

---
+++ _15722/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] dX = { 0, 1, 0, -1 };
+int[] dY = { 1, 0, -1, 0 };
+
+int n = int.Parse(Console.ReadLine()!);
+
+int cur = 0, count = 0, x = 0, y = 0;
+for(int i = 1; i <= n; i++)
+{
+    for(int j = 0; j < (i + 1) / 2; j++)
+    {
+        count++;
+        x += dX[cur];
+        y += dY[cur];
+
+        if(count == n)
+        {
+            Console.WriteLine($"{x} {y}");
+            return;
+        }
+    }
+    cur = (cur + 1) % 4;
+}
+
+Console.WriteLine($"{x} {y}");
