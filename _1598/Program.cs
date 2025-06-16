7c685fa881887de3854c1dbe8fa91314c10a9e3e
KSangMin <fhfpszmfnwm@naver.com>
2025-06-17 01:29:41 +0900

    [1598] 꼬리를 무는 숫자 나열

---
+++ _1598/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int a = ab[0] - 1, b = ab[1] - 1;
+int r1 = a / 4, c1 = a % 4;
+int r2 = b / 4, c2 = b % 4;
+int ans = Math.Abs(r1 - r2) + Math.Abs(c1 - c2);
+
+Console.WriteLine(ans);
