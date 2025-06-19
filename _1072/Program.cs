853be50a5ede043e3906cff6d9c0c927c25a96d6
KSangMin <fhfpszmfnwm@naver.com>
2025-06-19 16:26:14 +0900

    [1072] 게임

---
+++ _1072/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] xy = Console.ReadLine().Split().Select(int.Parse).ToArray();
+long x = xy[0], y = xy[1], z = y * 100 / x;
+
+if (z >= 99) Console.WriteLine("-1");
+else
+{
+    long left = 1, right = x, mid = (left + right) / 2;
+
+    while (left < right)
+    {
+        long ratio = (y + mid) * 100 / (x + mid);
+        if (ratio <= z) left = mid + 1;
+        else right = mid;
+
+        mid = (left + right) / 2;
+    }
+
+    Console.WriteLine(mid);
+}
