feb1041106e2b5f7b08ecdaa4929fb05bbbd477e
KSangMin <fhfpszmfnwm@naver.com>
2025-06-20 13:38:05 +0900

    [27648] 증가 배열 만들기

---
+++ _27648/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] nmk = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+long n = nmk[0], m = nmk[1], k = nmk[2];
+
+if(n + m - 1 <= k)
+{
+    int[] row = new int[m];
+    for(int i = 0; i < m; i++)
+    {
+        row[i] = i + 1;
+    }
+
+    Console.WriteLine("YES");
+    for(int i = 0; i < n; i++)
+    {
+        Console.WriteLine(string.Join(" ", row.Select(x => x + i)));
+    }
+}
+else Console.WriteLine("NO");
