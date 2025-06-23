5b9a18a214b3d5a7e35d22dacdd2bd2c3ac58d2d
KSangMin <fhfpszmfnwm@naver.com>
2025-06-23 15:23:22 +0900

    [4948] 베르트랑 공준

---
+++ _4948/Program.cs
@@ -0,0 +1,37 @@
+﻿bool[] prime = Enumerable.Repeat(true, 246913).ToArray();
+int n = -1;
+
+CheckPrime(prime);
+
+while (n != 0)
+{
+    n = int.Parse(Console.ReadLine());
+    if(n == 0) break;
+
+    Console.WriteLine(CountPrime(n));
+}
+
+void CheckPrime(bool[] prime)
+{
+    for(int i = 2; i * i < prime.Length; i++)
+    {
+        if(!prime[i]) continue;
+
+        for (int j = 1; j * i < prime.Length; j++)
+        {
+            prime[j * i] = false;
+        }
+    }
+}
+
+int CountPrime(int n)
+{
+    int count = 0;
+
+    for (int i = n + 1; i <= 2 * n; i++)
+    {
+        if(prime[i]) count++;
+    }
+
+    return count;
+}
