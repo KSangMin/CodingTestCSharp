788977b57d946b95b2e462b31cf41acf15cabfeb
KSangMin <fhfpszmfnwm@naver.com>
2025-06-23 18:53:35 +0900

    [4948] 베르트랑 공준 수정

--- _4948/Program.cs
+++ _4948/Program.cs
@@ -1,36 +1,29 @@
-﻿bool[] prime = Enumerable.Repeat(true, 246913).ToArray();
-int n = -1;
-
-CheckPrime(prime);
+﻿int n = -1;
 
 while (n != 0)
 {
     n = int.Parse(Console.ReadLine());
-    if(n == 0) break;
+    if (n == 0) break;
 
-    Console.WriteLine(CountPrime(n));
+    Console.WriteLine(FindPrime(n));
 }
 
-void CheckPrime(bool[] prime)
-{
-    for(int i = 2; i * i < prime.Length; i++)
-    {
-        if(!prime[i]) continue;
-
-        for (int j = 1; j * i < prime.Length; j++)
-        {
-            prime[j * i] = false;
-        }
-    }
-}
-
-int CountPrime(int n)
+int FindPrime(int n)
 {
     int count = 0;
 
     for (int i = n + 1; i <= 2 * n; i++)
     {
-        if(prime[i]) count++;
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) count++;
     }
 
     return count;
