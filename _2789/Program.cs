48816bfae5737426c766789c7702b79091120025
KSangMin <fhfpszmfnwm@naver.com>
2025-06-24 23:21:41 +0900

    [2789] 유학 금지

---
+++ _2789/Program.cs
@@ -0,0 +1,6 @@
+﻿char[] cam = {'C', 'A', 'M', 'B', 'R', 'I', 'D', 'G', 'E'};
+
+string input = Console.ReadLine();
+string result = new string(input.Where(c => !cam.Contains(c)).ToArray());
+
+Console.WriteLine(result);
