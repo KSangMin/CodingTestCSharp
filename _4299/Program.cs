9521bcbfedc7a2ae7a23653ca088808b192753b3
KSangMin <fhfpszmfnwm@naver.com>
2025-06-26 00:15:22 +0900

    [4299] AFC 윔블던

---
+++ _4299/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int a = ab[0], b = ab[1];
+if((a + b) % 2 == 1 || a < b)
+{
+    Console.WriteLine(-1);
+    return;
+}
+
+int c = (a + b) / 2, d = a - c;
+
+Console.WriteLine($"{c} {d}");
