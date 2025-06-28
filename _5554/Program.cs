cb8b1ffd284bc12486b1eb0b3cc15126f05c6920
KSangMin <fhfpszmfnwm@naver.com>
2025-06-28 23:09:56 +0900

    [5554] 심부름 가는 길

---
+++ _5554/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] times = new int[4];
+
+for(int i = 0; i < 4; i++)
+{
+    times[i] = int.Parse(Console.ReadLine());
+}
+
+int sum = times.Sum();
+Console.WriteLine(sum / 60);
+Console.WriteLine(sum % 60);
