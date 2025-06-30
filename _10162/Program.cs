2ee1340306f976e0cb912f84945ed162a54d80b3
KSangMin <fhfpszmfnwm@naver.com>
2025-06-30 18:43:52 +0900

    [10162] 전자레인지

---
+++ _10162/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] abc = new int[3];
+
+if (n % 10 != 0) Console.WriteLine(-1);
+else
+{
+    abc[0] = n / 300;
+    n %= 300;
+    abc[1] = n / 60;
+    n %= 60;
+    abc[2] = n / 10;
+
+    Console.WriteLine($"{abc[0]} {abc[1]} {abc[2]}");
+}
