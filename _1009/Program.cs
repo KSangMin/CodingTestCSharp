76e21cf969e62e388dfd6831aadaad59b17028b6
KSangMin <fhfpszmfnwm@naver.com>
2025-07-01 19:58:18 +0900

    [1009] 분산처리

---
+++ _1009/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    int[] ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    int a = ab[0], b = ab[1];
+
+    int result = a % 10;
+    for(int j = 0; j < b - 1; j++)
+    {
+        result = result * a % 10;
+    }
+
+    Console.WriteLine(result == 0 ? 10 : result);
+}
