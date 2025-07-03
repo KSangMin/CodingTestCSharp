8bacfeba6385a09bf6c246b261f3fa3eb142cdb7
KSangMin <fhfpszmfnwm@naver.com>
2025-07-03 23:13:59 +0900

    [2605] 줄 세우기

---
+++ _2605/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<int> st = new List<int>();
+int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+st.Add(1);
+for(int i = 2; i <= n; i++)
+{
+    st.Insert((int)MathF.Max(0, i - nums[i - 1] - 1), i);
+}
+
+Console.WriteLine(string.Join(' ', st));
