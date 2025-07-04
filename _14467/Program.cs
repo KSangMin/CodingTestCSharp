af09196fb122a4503c4d72766187be6ac7fe54b1
KSangMin <fhfpszmfnwm@naver.com>
2025-07-04 21:44:31 +0900

    [14467] 소가 길을 건너간 이유 1

---
+++ _14467/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Dictionary<int, int> cows = new Dictionary<int, int>();
+int ans = 0;
+
+for(int i = 0; i < n; i++)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    int cow = input[0], pos = input[1];
+    if (cows.ContainsKey(cow) && cows[cow] != pos) ans++;
+    cows[cow] = pos;
+}
+
+Console.WriteLine(ans);
