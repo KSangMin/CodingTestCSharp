f96118cfb181bdcfd4aef1ed9857a6d8dae46dc7
KSangMin <fhfpszmfnwm@naver.com>
2025-07-07 23:05:02 +0900

    [13118] 뉴턴과 사과

---
+++ _13118/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] people = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int[] xyr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+for(int i = 1; i <= people.Length; i++)
+{
+    if (people[i - 1] == xyr[0])
+    {
+        Console.WriteLine(i);
+        return;
+    }
+}
+
+Console.WriteLine(0);
