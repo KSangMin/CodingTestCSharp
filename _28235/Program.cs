afd9bcb68a24605d61ed1e9a627b15c0ba5de594
KSangMin <fhfpszmfnwm@naver.com>
2025-07-08 21:30:17 +0900

    [28235] 코드마스터 2023

---
+++ _28235/Program.cs
@@ -0,0 +1,11 @@
+﻿Dictionary<string, string> codeMaster = new Dictionary<string, string>
+{
+    { "SONGDO", "HIGHSCHOOL" },
+    { "CODE", "MASTER" },
+    { "2023", "0611" },
+    { "ALGORITHM", "CONTEST" }
+};
+
+string input = Console.ReadLine();
+
+Console.WriteLine(codeMaster[input]);
