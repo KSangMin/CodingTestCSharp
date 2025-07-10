0e4c8fcf3fd68a3a046906c9062819212fb7041d
KSangMin <fhfpszmfnwm@naver.com>
2025-07-10 22:48:53 +0900

    [17362] 수학은 체육과목 입니다 2

---
+++ _17362/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int ans = 0;
+
+switch (n % 8)
+{
+    case 1:
+        ans = 1;
+        break;
+    case 0:
+    case 2:
+        ans = 2;
+        break;
+    case 7:
+    case 3:
+        ans = 3;
+        break;
+    case 6:
+    case 4:
+        ans = 4;
+        break;
+    case 5:
+        ans = 5;
+        break;
+}
+
+Console.WriteLine(ans);
