f8fb17aea5fa29cd6668ab00d80d9d1f7a0a97a4
KSangMin <fhfpszmfnwm@naver.com>
2025-07-11 20:14:48 +0900

    [26068] 치킨댄스를 추는 곰곰이를 본 임스 2

---
+++ _26068/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int ans = 0;
+for(int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine();
+
+    if (int.Parse(input.Substring(2, input.Length - 2)) <= 90) ans++;
+}
+
+Console.WriteLine(ans);
