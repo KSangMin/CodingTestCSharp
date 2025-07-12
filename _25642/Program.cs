80964b11cede9dc61f5ea3beed5be0a8d46866df
KSangMin <fhfpszmfnwm@naver.com>
2025-07-13 00:38:29 +0900

    [25642] 젓가락 게임

---
+++ _25642/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] xy = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int x = xy[0], y = xy[1];
+
+bool ytPhase = true;
+while(x < 5 && y < 5)
+{
+    if (ytPhase) y += x;
+    else x += y;
+
+    ytPhase = !ytPhase;
+}
+
+Console.WriteLine(x < y ? "yt" : "yj");
