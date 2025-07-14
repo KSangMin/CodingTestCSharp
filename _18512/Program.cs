f2888ff49381c68a4fb0a5afbda204e56badccd9
KSangMin <fhfpszmfnwm@naver.com>
2025-07-15 00:34:26 +0900

    [18512] 점프 점프

---
+++ _18512/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] xy = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int x = xy[0], y = xy[1], p1 = xy[2], p2 = xy[3];
+
+int ans = 0;
+
+while(p1 <= 10000 && p2 <= 10000)
+{
+    if(p1 == p2)
+    {
+        Console.WriteLine(p1);
+        return;
+    }
+    else if(p1 < p2)
+    {
+        p1 += x;
+    }
+    else
+    {
+        p2 += y;
+    }
+}
+
+Console.WriteLine(-1);
