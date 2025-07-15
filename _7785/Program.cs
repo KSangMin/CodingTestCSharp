c8f1d900bc4b0d3288ea52354649f5106ccbc231
KSangMin <fhfpszmfnwm@naver.com>
2025-07-15 22:24:00 +0900

    [7785] 회사에 있는 사람

---
+++ _7785/Program.cs
@@ -0,0 +1,21 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+HashSet<string> people = new HashSet<string>();
+for(int i = 0; i < N; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    if (input[1] == "enter")
+    {
+        people.Add(input[0]);
+    }
+    else if (input[1] == "leave")
+    {
+        people.Remove(input[0]);
+    }
+}
+
+List<string> sortedPeople = people.ToList();
+sortedPeople.Sort();
+sortedPeople.Reverse();
+
+Console.WriteLine(String.Join("\n", sortedPeople));
