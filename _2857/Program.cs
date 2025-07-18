32ddc471a88fafec040a3c7668a1813618220e9c
KSangMin <fhfpszmfnwm@naver.com>
2025-07-19 00:16:29 +0900

    [2857] FBI

---
+++ _2857/Program.cs
@@ -0,0 +1,17 @@
+﻿List<int> count = new();
+
+for(int i = 0; i < 5; i++)
+{
+    List<string> input = Console.ReadLine().Split('-').ToList();
+
+    for(int j = 0; j < input.Count; j++)
+    {
+        if (input[j].Contains("FBI"))
+        {
+            count.Add(i + 1);
+            break;
+        }
+    }
+}
+
+Console.WriteLine(count.Count > 0 ? string.Join(' ', count) : "HE GOT AWAY!");
