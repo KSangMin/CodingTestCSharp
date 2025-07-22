3e0beb998162d83af9fe774c3c6d101f4f87642e
KSangMin <fhfpszmfnwm@naver.com>
2025-07-22 22:39:06 +0900

    [8949] 대충 더해

---
+++ _8949/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] ab = Console.ReadLine().Split();
+
+string a = ab[0], b = ab[1];
+
+int max = (int)MathF.Max(a.Length, b.Length);
+int[] nums = new int[max];
+
+for(int i = max - 1; i >= 0; i--)
+{
+    int aIdx = i - (max - a.Length);
+    int bIdx = i - (max - b.Length);
+
+    if (0 <= aIdx)
+    {
+        nums[i] += (a[aIdx] - '0');
+    }
+    if (0 <= bIdx)
+    {
+        nums[i] += (b[bIdx] - '0');
+    }
+}
+
+Console.WriteLine(string.Concat(nums));
