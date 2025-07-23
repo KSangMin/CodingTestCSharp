bfbcdb712ae24a0d0bfb807a6f365a376a541c2c
KSangMin <fhfpszmfnwm@naver.com>
2025-07-23 20:02:26 +0900

    [14425] 문자열 집합

---
+++ _14425/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] NM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int N = NM[0], M = NM[1];
+
+HashSet<string> S = new();
+
+for(int i = 0; i < N; i++)
+{
+    S.Add(Console.ReadLine());
+}
+
+int ans = 0;
+for(int i = 0; i < M; i++)
+{
+    string str = Console.ReadLine();
+    if (S.Contains(str))
+    {
+        ans++;
+    }
+}
+
+Console.WriteLine(ans);
