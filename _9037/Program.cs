77d81432e9580dabd528b4e9106826a8b56b1442
KSangMin <fhfpszmfnwm@naver.com>
2025-07-24 23:37:23 +0900

    [9037] The candy war

---
+++ _9037/Program.cs
@@ -0,0 +1,51 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < t; i++)
+{
+    int N = int.Parse(Console.ReadLine());
+    int[] childs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    int count = 0;
+
+    while (true)
+    {
+        bool clear = true;
+
+        for (int j = 0; j < N; j++)
+        {
+            if (childs[j] % 2 == 1)
+            {
+                childs[j]++;
+            }
+        }
+
+        for (int j = 1; j < N; j++)
+        {
+            if (childs[j] != childs[j - 1])
+            {
+                clear = false;
+            }
+        }
+
+        if (clear)
+        {
+            break;
+        }
+
+        count++;
+
+        int[] temp = new int[N];
+        for (int j = 0; j < N; j++)
+        {
+            temp[(j + 1) % N] += childs[j] >> 1;
+            childs[j] >>= 1;
+        }
+
+        for (int j = 0; j < N; j++)
+        {
+            childs[j] += temp[j];
+        }
+    }
+
+    Console.WriteLine(count);
+}
