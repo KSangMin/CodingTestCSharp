e58e10ea9ae7488092e17c717c3d699c482e7615
KSangMin <fhfpszmfnwm@naver.com>
2025-07-25 20:56:48 +0900

    [31880] K512컵 개최!

---
+++ _31880/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.ReadLine();
+
+int[] Ns = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] Ms = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+long ans = Ns.Sum();
+
+foreach(int m in Ms) if(m > 1) ans *= m;
+
+Console.WriteLine(ans);
