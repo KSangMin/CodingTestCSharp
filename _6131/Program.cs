9dc1f17013bde979c882f4a087046dc833d71824
KSangMin <fhfpszmfnwm@naver.com>
2025-07-27 21:06:57 +0900

    [6131] 완전 제곱수

---
+++ _6131/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int cnt = 0;
+for (int i = 1; i <= 999; i++)
+{
+    for(int j = i + 1; j <= 1000; j++)
+    {
+        int iSqr = i * i;
+        int jSqr = j * j;
+
+        if (iSqr + n == jSqr)
+        {
+            cnt++;
+        }
+        else if (iSqr + n < jSqr)
+        {
+            break;
+        }
+    }
+}
+
+Console.WriteLine(cnt);
