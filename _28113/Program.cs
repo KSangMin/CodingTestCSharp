333a27e9c0b6df785ffbb6829234bad8fa1f23bc
KSangMin <fhfpszmfnwm@naver.com>
2025-07-29 22:36:41 +0900

    [28113] 정보섬의 대중교통

---
+++ _28113/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] NAB = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int N = NAB[0], A = NAB[1], B = NAB[2];
+
+if(N <= B)
+{
+    if(A< B)
+    {
+        Console.WriteLine("Bus");
+    }
+    else if (A>B)
+    {
+        Console.WriteLine("Subway");
+    }
+    else
+    {
+        Console.WriteLine("Anything");
+    }
+}
+else
+{
+    Console.WriteLine("Bus");
+}
