c62b33a198963b6cf56143b04322b4c454b8b7ae
KSangMin <fhfpszmfnwm@naver.com>
2025-07-30 21:56:41 +0900

    [17173] 배수들의 합

---
+++ _17173/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] NM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int N = NM[0], M = NM[1];
+
+int[] Ks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int ans = 0;
+for (int i = 1; i <= N; i++)
+{
+    for(int j = 0; j < M; j++)
+    {
+        if (i % Ks[j] == 0)
+        {
+            ans += i;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(ans);
