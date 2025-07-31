eb7a19657ec2011b8fb1fe8df482fb477fc1092a
KSangMin <fhfpszmfnwm@naver.com>
2025-07-31 19:50:48 +0900

    [14929] 귀찮아 (SIB)

---
+++ _14929/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+long sum = 0;
+long ans = 0;
+for(int i = n - 1; i >= 0; i--)
+{
+    ans += nums[i] * sum;
+    sum += nums[i];
+}
+
+Console.WriteLine(ans);
