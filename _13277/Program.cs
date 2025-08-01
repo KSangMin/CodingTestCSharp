47926de3d9a0aedae5c01cf102c5e4aa7b86838b
KSangMin <fhfpszmfnwm@naver.com>
2025-08-02 02:12:27 +0900

    [10569] 다면체

---
+++ _13277/Program.cs
@@ -0,0 +1,6 @@
+﻿int T = int.Parse(Console.ReadLine());
+for(int i = 0; i < T; i++)
+{
+    int[] VE = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    Console.WriteLine(2 - VE[0] + VE[1]);
+}
