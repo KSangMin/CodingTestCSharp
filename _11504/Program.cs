194559187ea1886a83ef198651169d764e363191
KSangMin <fhfpszmfnwm@naver.com>
2025-08-03 01:36:06 +0900

    [11504] 돌려 돌려 돌림판!

---
+++ _11504/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+int T = int.Parse(Console.ReadLine());
+
+StringBuilder sb = new();
+
+for(int i = 0; i < T; i++)
+{
+    int[] NM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    int turnLen = NM[0], XYLen = NM[1];
+    int X = int.Parse(Console.ReadLine().Replace(" ", ""));
+    int Y = int.Parse(Console.ReadLine().Replace(" ", ""));
+
+    string allTurn = Console.ReadLine().Replace(" ", "");
+
+    int count = 0;
+    for(int j = 0; j < turnLen; j++)
+    {
+        int result = allTurn[j] - '0';
+        for(int k = 1; k < XYLen; k++)
+        {
+            result = result * 10 + allTurn[(j + k) % turnLen] - '0';
+        }
+
+        if(X <= result && result <= Y)
+        {
+            count++;
+        }
+    }
+
+    sb.AppendLine(count.ToString());
+}
+
+Console.Write(sb);
