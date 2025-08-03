1264a6076bd9413146ae0bd02642681fcebbfb9b
KSangMin <fhfpszmfnwm@naver.com>
2025-08-03 22:57:24 +0900

    [1453] 피시방 알바

---
+++ _1453/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.ReadLine();
+int[] people = Console.ReadLine().Split().Select(int.Parse).ToArray();
+bool[] check = new bool[101];
+
+int cnt = 0;
+for(int i = 0; i < people.Length; i++)
+{
+    if (check[people[i]] == true) cnt++;
+    else check[people[i]] = true;
+}
+
+Console.WriteLine(cnt);
