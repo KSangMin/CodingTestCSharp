dfcbb1e5956eb54e8fc701a30b143567447eaed4
KSangMin <fhfpszmfnwm@naver.com>
2025-08-04 19:00:13 +0900

    [27160] 할리갈리

---
+++ _27160/Program.cs
@@ -0,0 +1,28 @@
+﻿int N = int.Parse(Console.ReadLine());
+Dictionary<string, int> wordCount = new();
+
+bool Yes = false;
+for(int i = 0; i < N; i++)
+{
+    string[] input = Console.ReadLine().Split();
+
+    if(!wordCount.ContainsKey(input[0]))
+    {
+        wordCount[input[0]] = int.Parse(input[1]);
+    }
+    else
+    {
+        wordCount[input[0]] += int.Parse(input[1]);
+    }
+}
+
+foreach(var count in wordCount.Values)
+{
+    if(count == 5)
+    {
+        Yes = true;
+        break;
+    }
+}
+
+Console.WriteLine(Yes ? "YES" : "NO");
