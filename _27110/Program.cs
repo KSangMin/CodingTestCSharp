6a117a2ec5507a1ed10f2ceddcadbf045c292469
KSangMin <fhfpszmfnwm@naver.com>
2025-08-06 20:26:21 +0900

    [27110] 특식 배부

---
+++ _27110/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] ABC = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int ans = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    if (ABC[i] >= n)
+    {
+        ans += n;
+    }
+    else
+    {
+        ans += ABC[i];
+    }
+}
+
+Console.WriteLine(ans);
