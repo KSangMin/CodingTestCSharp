2e0a21a6f6ef2d7a9714dceb269a5084340c77b2
KSangMin <fhfpszmfnwm@naver.com>
2025-08-07 20:25:57 +0900

    [11971] 속도 위반

--- _11971/Program.cs
+++ _11971/Program.cs
@@ -1,4 +1,34 @@
 ﻿int[] NM = Console.ReadLine().Split().Select(int.Parse).ToArray();
 int N = NM[0], M = NM[1];
 
-Console.WriteLine(N / 2 < M / 2 ? N / 2 : M / 2);
+List<int> limit = new();
+List<int> speed = new();
+
+for (int i = 0; i < N; i++)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    for(int j = 0; j < input[0]; j++)
+    {
+        limit.Add(input[1]);
+    }
+}
+for (int i = 0; i < M; i++)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    for (int j = 0; j < input[0]; j++)
+    {
+        speed.Add(input[1]);
+    }
+}
+
+int max = 0;
+
+for (int i = 0; i < 100; i++)
+{
+    if (speed[i] > limit[i])
+    {
+        max = Math.Max(max, speed[i] - limit[i]);
+    }
+}
+
+Console.WriteLine(max);
