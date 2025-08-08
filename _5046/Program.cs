5cf4fbd99b48b40fa651ef28e5d1488aafa745e9
KSangMin <fhfpszmfnwm@naver.com>
2025-08-08 19:50:16 +0900

    [5046] 전국 대학생 프로그래밍 대회 동아리 연합

---
+++ _5046/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] NBHW = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int N = NBHW[0], B = NBHW[1], H = NBHW[2], W = NBHW[3];
+
+int min = 500001;
+for(int i = 0; i < H; i++)
+{
+    int cost = int.Parse(Console.ReadLine());
+    int[] people = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    
+    int total = cost * N;
+    if (total > B) continue;
+
+    foreach(int p in people)
+    {
+        if(p >= N)
+        {
+            min = Math.Min(min, total);
+            break;
+        }
+    }
+}
+
+Console.WriteLine(min == 500001 ? "stay home" : min);
