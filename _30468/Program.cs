ca2d7cc5896d1eeb3e4794c23d1aec2b92aea276
KSangMin <fhfpszmfnwm@naver.com>
2025-08-13 23:09:57 +0900

    [30468] 호반우가 학교에 지각한 이유 1

---
+++ _30468/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] stat = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int N = stat[4], sum = stat.Sum() - N;
+float avg = sum / 4f;
+
+Console.WriteLine(N > avg ? (N - avg) * 4 : 0);
