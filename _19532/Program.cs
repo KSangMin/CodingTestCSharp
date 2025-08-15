b7bdbbe6977cda58931411539292fb4ca3641961
KSangMin <fhfpszmfnwm@naver.com>
2025-08-16 00:28:58 +0900

    [19532] 수학은 비대면강의입니다

---
+++ _19532/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] abcdef = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int a = abcdef[0], b = abcdef[1], c = abcdef[2], d = abcdef[3], e = abcdef[4], f = abcdef[5];
+
+int x = (c * e - b * f) / (a * e - b * d);
+int y = (a * f - c * d) / (a * e - b * d);
+
+Console.WriteLine($"{x} {y}");
