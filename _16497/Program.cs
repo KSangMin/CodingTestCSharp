0968477a79746fb0e26dfc27f16621c2961522fc
KSangMin <fhfpszmfnwm@naver.com>
2025-08-17 00:20:57 +0900

    [16497] 대출 요청

---
+++ _16497/Program.cs
@@ -0,0 +1,35 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<(int start, int end)> intervals = new List<(int, int)>();
+for (int i = 0; i < n; i++)
+{
+    int[] ab = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    intervals.Add((ab[0], ab[1]));
+}
+
+intervals.Sort((a, b) => a.start.CompareTo(b.start));
+
+int K = int.Parse(Console.ReadLine());
+
+int[] last = new int[K];
+
+bool flag = true;
+foreach (var (s, e) in intervals)
+{
+    for(int j = 0; j < K; j++)
+    {
+        if (last[j] <= s)
+        {
+            last[j] = e;
+            break;
+        }
+        else if (j == K - 1) flag = false;
+    }
+
+    if (!flag)
+    {
+        break;
+    }
+}
+
+Console.WriteLine(flag ? 1 : 0);
