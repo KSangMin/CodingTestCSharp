12ac5ccab07c158f891ad4401cd4b5c133ad191c
KSangMin <fhfpszmfnwm@naver.com>
2025-08-20 20:47:21 +0900

    [30993] 자동차 주차

---
+++ _30993/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] NABC = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int N = NABC[0], A = NABC[1], B = NABC[2], C = NABC[3];
+
+long sum = factorial(N) / factorial(A) / factorial(B) / factorial(C);
+Console.WriteLine(sum);
+
+long factorial(int n)
+{
+    long result = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
