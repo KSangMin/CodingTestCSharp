76713fce7cf5b45147a4f2fcea3565d4dd80fc2f
KSangMin <fhfpszmfnwm@naver.com>
2025-08-21 20:25:34 +0900

    [14916] 거스름돈

---
+++ _14916/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int sum = n / 5;
+n %= 5;
+
+if(sum == 0 && n % 2 == 1)
+{
+    Console.WriteLine(-1);
+}
+else
+{
+    if (n == 1)
+    {
+        sum += 2;
+    }
+    else if(n == 3)
+    {
+        sum += 3;
+    }
+    else
+    {
+        sum += n / 2;
+    }
+
+    Console.WriteLine(sum);
+}
