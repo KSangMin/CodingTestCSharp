6210b8699b4d53ad859afa318b0f5cf4345f61f7
KSangMin <fhfpszmfnwm@naver.com>
2025-08-23 22:12:49 +0900

    [25177] 서강의 역사를 찾아서

---
+++ _25177/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] NM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int N = NM[0], M = NM[1];
+
+int[] cur = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] prev = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int max = 0;
+for(int i = 0; i < Math.Max(N, M); i++)
+{
+    int curP = 0, prevP = 0;
+
+    if(i < N) curP = cur[i];
+    if(i < M) prevP = prev[i];
+
+    max = Math.Max(max, prevP - curP);
+}
+
+Console.WriteLine(max);
