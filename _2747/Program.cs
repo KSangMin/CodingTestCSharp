cbb0373efc5cd7727095d91f2729595f57c88b75
KSangMin <fhfpszmfnwm@naver.com>
2025-08-24 21:45:06 +0900

    [2747] 피보나치 수

---
+++ _2747/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<int> fib = new List<int>() { 0, 1 };
+for(int i = 2; i <= n; i++)
+{
+    fib.Add(fib[i - 1] + fib[i - 2]);
+}
+Console.WriteLine(fib[n]);
