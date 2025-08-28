64725b93605742e4f87374364fb7dcfaf7884b62
KSangMin <fhfpszmfnwm@naver.com>
2025-08-28 18:59:58 +0900

    [32779] 가희와 전기 요금 1

---
+++ _32779/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    int[]AM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    long a = AM[0], m = AM[1];
+
+    Console.WriteLine((a * m * 176 / 100000));
+}
