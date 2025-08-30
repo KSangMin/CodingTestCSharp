22e7468f9c2fbbf722fb8399972fa05db1fb0e6d
KSangMin <fhfpszmfnwm@naver.com>
2025-08-30 23:15:38 +0900

    [2386] 도비의 영어 공부

---
+++ _2386/Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input[0] == '#')
+    {
+        break;
+    }
+
+    char target = input[0];
+    string sentence = input.Substring(2);
+    int count = 0;
+    foreach (char c in sentence)
+    {
+        if (c == target || c - 'A' + 'a' == target)
+        {
+            count++;
+        }
+    }
+
+    Console.WriteLine($"{target} {count}");
+}
