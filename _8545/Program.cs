83cedde1e73858ed51cd1d2fe8e55779c4c33963
KSangMin <fhfpszmfnwm@naver.com>
2025-09-02 19:22:05 +0900

    [8545] Zadanie próbne

---
+++ _8545/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+string abc = Console.ReadLine();
+
+StringBuilder sb = new();
+for(int i = 0; i < abc.Length; i++)
+{
+    sb.Append(abc[abc.Length - i - 1]);
+}
+
+Console.WriteLine(sb.ToString());
