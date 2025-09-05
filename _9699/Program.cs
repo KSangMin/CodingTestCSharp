3b905e266cf5d01a469f773fecd424f1a1f5daa1
KSangMin <fhfpszmfnwm@naver.com>
2025-09-06 00:14:06 +0900

    [9699] RICE SACK

---
+++ _9699/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    int[] rice = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    Console.WriteLine($"Case #{i + 1}: {rice.Max()}");
+}
