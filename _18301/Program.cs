4919aead455a1641b84c4a5ee62a7a1539e4fb72
KSangMin <fhfpszmfnwm@naver.com>
2025-09-07 00:46:24 +0900

    [18301] Rats

---
+++ _18301/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] n1212 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n1 = n1212[0], n2 = n1212[1], n12 = n1212[2];
+
+Console.WriteLine((int)((n1 + 1) * (n2 + 1) / (n12 + 1)) - 1);
