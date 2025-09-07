ab2db538e29dc8787638e08dd7575385c5458681
KSangMin <fhfpszmfnwm@naver.com>
2025-09-07 22:55:45 +0900

    [1100] 하얀 칸

---
+++ _2238/Program.cs
@@ -0,0 +1,16 @@
+﻿int count = 0;
+
+for(int i = 0; i < 8; i++)
+{
+    string s = Console.ReadLine();
+
+    for(int j = 0; j < s.Length; j++)
+    {
+        if((i + j) % 2 == 0 && s[j] == 'F')
+        {
+            count++;
+        }
+    }
+}
+
+Console.WriteLine(count);
