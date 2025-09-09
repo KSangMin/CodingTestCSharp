8d797730dfaace393fa2e0d5c4e7c1435933ec56
KSangMin <fhfpszmfnwm@naver.com>
2025-09-09 19:56:11 +0900

    [32762] 더치 페이

---
+++ _32762/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n = nm[0], m = nm[1];
+
+for (int i = 0; i < n; i++)
+{
+    Console.ReadLine();
+}
+
+int sum = 0;
+for (int i = 0; i < m; i++)
+{
+    string[] tp = Console.ReadLine().Split();
+    sum += int.Parse(tp[1]);
+}
+
+Console.WriteLine($"{sum / (float)n:F4}");
