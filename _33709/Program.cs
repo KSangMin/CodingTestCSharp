6352e914663479580b1993a045165127fd700a0c
KSangMin <fhfpszmfnwm@naver.com>
2025-09-10 19:13:36 +0900

    [33709] 치매예방수칙 3.3.3

---
+++ _33709/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+
+string s = Console.ReadLine()!;
+
+int sum = 0;
+StringBuilder sb = new();
+foreach (char c in s)
+{
+    if (c == '.' || c == '|' || c == ':' || c == '#')
+    {
+        sum += int.Parse(sb.ToString());
+        sb.Clear();
+    }
+    else
+    {
+        sb.Append(c);
+    }
+}
+
+Console.WriteLine(sum + int.Parse(sb.ToString()));
