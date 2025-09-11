ecc81aee3d4e9e7d69af9e901a97989ebc88c870
KSangMin <fhfpszmfnwm@naver.com>
2025-09-11 19:42:36 +0900

    [9093] 단어 뒤집기

---
+++ _9093/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+
+    StringBuilder sb = new();
+    foreach(string s in input)
+    {
+        for(int j = s.Length - 1; j >= 0; j--)
+        {
+            sb.Append(s[j]);
+        }
+        sb.Append(' ');
+    }
+
+    Console.WriteLine(sb.ToString());
+}
