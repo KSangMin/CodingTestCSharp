1f451211dece9b82921b7613b855648d5f089e2d
KSangMin <fhfpszmfnwm@naver.com>
2025-09-14 21:15:42 +0900

    [31882] 근수

---
+++ _31882/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.ReadLine();
+
+string s = Console.ReadLine();
+
+long count = 0;
+long result = 0;
+for (int i = 0; i < s.Length; i++)
+{
+    if (s[i] == '2')
+    {
+        count++;
+    }
+    else
+    {
+        result += Calc(count);
+        count = 0;
+    }
+}
+
+result += Calc(count);
+
+Console.WriteLine(result);
+
+long Calc(long n)
+{
+    long sum = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        sum += i * (n + 1 - i);
+    }
+
+    return sum;
+}
