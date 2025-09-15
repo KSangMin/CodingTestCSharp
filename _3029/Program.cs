9cb280797f230a8e568d356eeeef255c8f374d9f
KSangMin <fhfpszmfnwm@naver.com>
2025-09-15 19:04:18 +0900

    [3029] 경고

---
+++ _3029/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] start = Console.ReadLine()!.Split(':').Select(int.Parse).ToArray();
+int[] end = Console.ReadLine()!.Split(':').Select(int.Parse).ToArray();
+
+int[] diff = new int[3];
+
+int s = start[0] * 3600 + start[1] * 60 + start[2];
+int e = end[0] * 3600 + end[1] * 60 + end[2];
+
+int d = e - s;
+if(d <= 0) d += 24 * 3600;
+
+diff[0] = d / 3600;
+d %= 3600;
+diff[1] = d / 60;
+diff[2] = d % 60;
+
+Console.WriteLine($"{diff[0]:D2}:{diff[1]:D2}:{diff[2]:D2}");
