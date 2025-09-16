9a64fa77131fd3dcab7fbef97f6486320f994b97
KSangMin <fhfpszmfnwm@naver.com>
2025-09-17 00:04:27 +0900

    [5341] Pyramids

---
+++ _5341/Program.cs
@@ -0,0 +1,23 @@
+﻿List<int> sum = new() { 0, 1 };
+int min = 1;
+
+while (true)
+{
+    int n = int.Parse(Console.ReadLine()!);
+
+    if(n == 0)
+    {
+        break;
+    }
+
+    if(n > min)
+    {
+        for(int i = min + 1; i <= n; i++)
+        {
+            sum.Add(sum[i - 1] + i);
+        }
+        min = n;
+    }
+
+    Console.WriteLine(sum[n]);
+}
