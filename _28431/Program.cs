77a9af35907b40125c2909b05eb673f8e8731c95
KSangMin <fhfpszmfnwm@naver.com>
2025-09-18 22:56:07 +0900

    [28431] 양말 짝 맞추기

---
+++ _28431/Program.cs
@@ -0,0 +1,17 @@
+﻿List<int> nums = new();
+
+for(int i = 0; i < 5; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+
+    if(!nums.Contains(n))
+    {
+        nums.Add(n);
+    }
+    else
+    {
+        nums.Remove(n);
+    }
+}
+
+Console.WriteLine(nums[0]);
