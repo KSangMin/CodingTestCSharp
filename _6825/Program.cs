a89256b3ca9a6c39be255a8b52cdcb181c74e64d
KSangMin <fhfpszmfnwm@naver.com>
2025-09-20 23:55:07 +0900

    [6825] Body Mass Index

---
+++ _6825/Program.cs
@@ -0,0 +1,8 @@
+﻿float w = float.Parse(Console.ReadLine()!);
+float h = float.Parse(Console.ReadLine()!);
+
+float bmi = w / h / h;
+
+if(bmi < 18.5f) Console.WriteLine("Underweight");
+else if(bmi <= 25.0f) Console.WriteLine("Normal weight");
+else Console.WriteLine("Overweight");
