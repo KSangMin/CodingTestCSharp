d3b7ba1180c9325a047df4d0559a3914c5d15040
KSangMin <fhfpszmfnwm@naver.com>
2025-09-21 21:13:30 +0900

    [5357] Dedupe

---
+++ _5357/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+
+for(int i = 0; i < n; i++)
+{
+    string s = Console.ReadLine()!;
+    StringBuilder sb = new();
+
+    char prev = s[0];
+    sb.Append(prev);
+    for(int j = 1; j < s.Length; j++)
+    {
+        if(s[j] != prev)
+        {
+            sb.Append(s[j]);
+            prev = s[j];
+        }
+    }
+
+    Console.WriteLine(sb.ToString());
+}
