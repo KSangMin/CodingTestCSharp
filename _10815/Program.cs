8edaab45e3a68377497c73695987fc9323fe2e12
KSangMin <fhfpszmfnwm@naver.com>
2025-09-22 22:15:50 +0900

    [10815] 숫자 카드

---
+++ _10815/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+HashSet<int> card = Console.ReadLine().Split().Select(int.Parse).ToHashSet<int>();
+
+int m = int.Parse(Console.ReadLine());
+int[] check = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+StringBuilder sb = new();
+foreach(int c in check)
+{
+    sb.Append(card.Contains(c) ? "1 " : "0 ");
+}
+
+Console.WriteLine(sb.ToString());
