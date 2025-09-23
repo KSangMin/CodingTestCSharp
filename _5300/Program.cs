9c8c1009e862b712ecde4adb4b38002051f8e4db
KSangMin <fhfpszmfnwm@naver.com>
2025-09-23 21:34:08 +0900

    [5300] Fill the Rowboats!

---
+++ _5300/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+
+sb.Append('1');
+for(int i = 2; i <= n; i++)
+{
+    sb.Append($" {i}");
+
+    if(i % 6 == 0)
+    {
+        sb.Append(" Go!");
+    }
+}
+
+if(n % 6 != 0)
+{
+    sb.Append(" Go!");
+}
+
+Console.WriteLine(sb.ToString());
