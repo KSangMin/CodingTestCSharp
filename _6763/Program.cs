05adf1c55d14bf75be65f95e05c2f0f47e7f5575
KSangMin <fhfpszmfnwm@naver.com>
2025-09-25 23:31:47 +0900

    [6763] Speed fines are not fine!

---
+++ _6763/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int m = int.Parse(Console.ReadLine()!);
+
+if(m > n)
+{
+    int diff = m - n;
+    int fine = 0;
+    if(diff <= 20)
+    {
+        fine = 100;
+    }
+    else if (diff <= 30)
+    {
+        fine = 270;
+    }
+    else
+    {
+        fine = 500;
+    }
+    Console.WriteLine($"You are speeding and your fine is ${fine}.");
+}
+else
+{
+    Console.WriteLine("Congratulations, you are within the speed limit!");
+}
