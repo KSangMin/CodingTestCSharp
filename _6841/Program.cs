3134affd89b33753dc9ee031aca00cf31483912e
KSangMin <fhfpszmfnwm@naver.com>
2025-09-27 00:39:35 +0900

    [6841] I Speak TXTMSG

---
+++ _6841/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Text;
+
+Dictionary<string, string> TXTMSG = new()
+{
+    { "CU", "see you" },
+    { ":-)", "I’m happy" },
+    { ":-(", "I’m unhappy" },
+    { ";-)", "wink" },
+    { ":-P", "stick out my tongue" },
+    { "(~.~)", "sleepy" },
+    { "TA", "totally awesome" },
+    { "CCC", "Canadian Computing Competition" },
+    { "CUZ", "because" },
+    { "TY", "thank-you" },
+    { "YW", "you’re welcome" },
+    { "TTYL", "talk to you later" },
+};
+
+StringBuilder sb = new();
+
+while (true)
+{
+    string input = Console.ReadLine()!;
+
+    sb.AppendLine(TXTMSG.ContainsKey(input) ? TXTMSG[input] : input);
+
+    if (input == "TTYL")
+    {
+        break;
+    }
+}
+
+Console.WriteLine(sb.ToString());
