1d06ee439f11d34b001314de994f864625353c7b
KSangMin <fhfpszmfnwm@naver.com>
2025-09-29 01:14:59 +0900

    [8760] Schronisko

---
+++ _8760/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+int z = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for (int i = 0; i < z; i++)
+{
+    int[] wk = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    int w = wk[0], k = wk[1];
+
+    int ans = (w / 2) * (k / 2) * 2;
+    if (w % 2 == 1)
+    {
+        ans += k / 2;
+    }
+    if (k % 2 == 1)
+    {
+        ans += w / 2;
+    }
+
+    sb.AppendLine(ans.ToString());
+}
+
+Console.Write(sb.ToString());
