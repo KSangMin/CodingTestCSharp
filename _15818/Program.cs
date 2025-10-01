32bb9a8fe6e8a7c64d1b302abd86b6bd23d4e627
KSangMin <fhfpszmfnwm@naver.com>
2025-10-02 00:15:08 +0900

    [15818] 오버플로우와 모듈러

---
+++ _15818/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] NM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int m = NM[1];
+
+int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+long ans = nums[0] % m;
+
+for (int i = 1; i < nums.Length; i++)
+{
+    ans = (ans * (nums[i] % m)) % m;
+}
+
+Console.WriteLine(ans);
