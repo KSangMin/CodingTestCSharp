45437359c0518f260a5ff89e5d623c1fd705aee5
KSangMin <fhfpszmfnwm@naver.com>
2025-10-03 01:39:56 +0900

    [5704] 팬그램

---
+++ _5704/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+StringBuilder sb = new();
+
+while (true)
+{
+    string s = Console.ReadLine()!;
+    if(s == "*")
+    {
+        break;
+    }
+
+    if(s.Length < 26)
+    {
+        sb.AppendLine("N");
+    }
+    else
+    {
+        bool[] check = new bool[26];
+
+        foreach (char c in s)
+        {
+            if (c != ' ')
+            {
+                check[c - 'a'] = true;
+            }
+        }
+
+        bool flag = true;
+        foreach(bool b in check)
+        {
+            if (!b)
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        sb.AppendLine(flag ? "Y" : "N");
+    }
+}
+
+Console.Write(sb.ToString());
