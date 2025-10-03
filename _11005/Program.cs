eafd57c7201042e85e479d3054a766ec943faa96
KSangMin <fhfpszmfnwm@naver.com>
2025-10-03 22:12:48 +0900

    [11005] 진법 변환 2

---
+++ _11005/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+int[] NB = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int n = NB[0], b = NB[1];
+
+StringBuilder sb = new();
+while (n > 0)
+{
+    int a = n % b;
+    sb.Insert(0, a > 9 ? (char)('A' + a - 10) : (char)('0' + a));
+
+    n /= b;
+}
+
+Console.WriteLine(sb.ToString());
