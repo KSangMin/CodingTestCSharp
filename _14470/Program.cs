573e654bd9e62319977a9e77530e24a0a36d978e
KSangMin <fhfpszmfnwm@naver.com>
2025-10-05 01:15:41 +0900

    [14470] 전자레인지

---
+++ _14470/Program.cs
@@ -0,0 +1,31 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int d = int.Parse(Console.ReadLine()!);
+int e = int.Parse(Console.ReadLine()!);
+
+bool f = a < 0;
+int t = 0;
+while(a < b)
+{
+    if (f)
+    {
+        if(a < 0)
+        {
+            t += c;
+            a++;
+        }
+        else if(a == 0)
+        {
+            t += d;
+            f = false;
+        }
+    }
+    else
+    {
+        t += e;
+        a++;
+    }
+}
+
+Console.WriteLine(t);
