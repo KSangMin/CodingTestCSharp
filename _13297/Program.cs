8a4c1094b4e84ba41559fc99949e41eb0fa78e66
KSangMin <fhfpszmfnwm@naver.com>
2025-10-07 19:58:45 +0900

    [13297] Quick Estimates

---
+++ _13297/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for(int i = 0; i < n; i++)
+{
+    string s = Console.ReadLine()!;
+    sb.AppendLine(s.Length.ToString());
+}
+
+Console.Write(sb.ToString());
