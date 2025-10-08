2713d8ef831b5d42f9a6d4c6ae700c2d052e611d
KSangMin <fhfpszmfnwm@naver.com>
2025-10-08 21:42:35 +0900

    [25641] 균형 잡힌 소떡소떡

---
+++ _25641/Program.cs
@@ -0,0 +1,29 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+string ts = Console.ReadLine()!;
+
+int tCount = ts.Count(c => c == 't');
+int sCount = ts.Count(c => c == 's');
+int id = 0;
+for (int i = 0; i < n - 1; i++)
+{
+    if(tCount == sCount)
+    {
+        break;
+    }
+
+    char each = ts[i];
+    switch(each)
+    {
+        case 't':
+            tCount--;
+            break;
+        case 's':
+            sCount--;
+            break;
+    }
+
+    id++;
+}
+
+Console.WriteLine(ts.Substring(id));
