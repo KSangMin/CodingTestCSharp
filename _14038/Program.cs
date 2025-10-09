3d4201285912cabf786c343dfeac4741b2171661
KSangMin <fhfpszmfnwm@naver.com>
2025-10-09 23:20:39 +0900

    [14038] Tournament Selection

---
+++ _14038/Program.cs
@@ -0,0 +1,29 @@
+﻿int count = 0;
+
+for(int i = 0; i < 6; i++)
+{
+    char c = Console.ReadLine()![0];
+    if(c == 'W')
+    {
+        count++;
+    }
+}
+
+switch(count)
+{
+    case 0:
+        Console.WriteLine(-1);
+        break;
+    case 1:
+    case 2:
+        Console.WriteLine(3);
+        break;
+    case 3:
+    case 4:
+        Console.WriteLine(2);
+        break;
+    case 5:
+    case 6:
+        Console.WriteLine(1);
+        break;
+}
