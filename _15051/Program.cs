b4ffd6f1f17585a65d3fb3df982f83536e978fa1
KSangMin <fhfpszmfnwm@naver.com>
2025-10-12 21:29:47 +0900

    [15051] Máquina de café

---
+++ _15051/Program.cs
@@ -0,0 +1,7 @@
+﻿int a1 = int.Parse(Console.ReadLine()!);
+int a2 = int.Parse(Console.ReadLine()!);
+int a3 = int.Parse(Console.ReadLine()!);
+
+int ans = Math.Min(a2 + a3 * 2, Math.Min(a1 + a3, a1 * 2 + a2));
+
+Console.WriteLine(ans * 2);
