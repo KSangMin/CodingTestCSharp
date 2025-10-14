3c5039411f1c662d778a99430028a12669b3dc5b
KSangMin <fhfpszmfnwm@naver.com>
2025-10-14 21:29:04 +0900

    [15059] Hard choice

---
+++ _15059/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] had = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int[] asked = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int ans = 0;
+ans += Math.Max(0, asked[0] - had[0]);
+ans += Math.Max(0, asked[1] - had[1]);
+ans += Math.Max(0, asked[2] - had[2]);
+
+Console.WriteLine(ans);
