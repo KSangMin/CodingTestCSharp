e9e896f4188f0bfdca26ac70039cc9d9686fb30d
KSangMin <fhfpszmfnwm@naver.com>
2025-10-15 20:04:14 +0900

    [15000] CAPS

---
+++ _15000/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+string s = Console.ReadLine()!;
+
+StringBuilder sb = new();
+foreach (char c in s)
+{
+    sb.Append((char)(c - 'a' + 'A'));
+}
+
+Console.WriteLine(sb.ToString());
