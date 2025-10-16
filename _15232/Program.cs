855dc3b895845ffb443e9cb5b29da531d0efb62c
KSangMin <fhfpszmfnwm@naver.com>
2025-10-16 17:05:19 +0900

    [15232] Rectangles

---
+++ _15232/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+int r = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+string s = new('*', c);
+
+for (int i = 0; i < r; i++)
+{
+    Console.WriteLine(s);
+}
