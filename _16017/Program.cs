f3e3e7ab05578996678b7c8b5dcb48ffdb32126f
KSangMin <fhfpszmfnwm@naver.com>
2025-10-19 22:59:32 +0900

    [16017] Telemarketer or not?

---
+++ _16017/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int d = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine((a == 8 || a == 9) && (d == 8 || d == 9) && (b == c) ? "ignore" : "answer");
