bc6dc8f6eaebf55240dd04afe937ced387ade8d2
KSangMin <fhfpszmfnwm@naver.com>
2025-10-20 23:54:41 +0900

    [17009] Winning Score

---
+++ _17009/Program.cs
@@ -0,0 +1,11 @@
+﻿int apple3 = int.Parse(Console.ReadLine()!);
+int apple2 = int.Parse(Console.ReadLine()!);
+int apple1 = int.Parse(Console.ReadLine()!);
+int banana3 = int.Parse(Console.ReadLine()!);
+int banana2 = int.Parse(Console.ReadLine()!);
+int banana1 = int.Parse(Console.ReadLine()!);
+
+int apple = apple3 * 3 + apple2 * 2 + apple1;
+int banana = banana3 * 3 + banana2 * 2 + banana1;
+
+Console.WriteLine(apple > banana ? "A" : apple < banana ? "B" : "T");
