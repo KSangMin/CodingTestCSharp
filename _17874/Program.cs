fe680479f7f576402ac8d81516e6e51b19a361d0
KSangMin <fhfpszmfnwm@naver.com>
2025-10-27 03:24:57 +0900

    [17874] Piece of Cake!

---
+++ _17874/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] nhv = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int n = nhv[0], h = nhv[1], v = nhv[2];
+
+int H = Math.Max(h, n - h);
+int V = Math.Max(v, n - v);
+Console.WriteLine(H * V * 4);
