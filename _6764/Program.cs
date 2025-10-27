1f71c52e90d87ec14ca6dbce97406918fc656818
KSangMin <fhfpszmfnwm@naver.com>
2025-10-27 21:50:16 +0900

    [6764] Sounds fishy!

---
+++ _6764/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int d = int.Parse(Console.ReadLine()!);
+
+if(a > b && b > c && c > d)
+{
+    Console.WriteLine("Fish Diving");
+}
+else if(a < b && b < c && c < d)
+{
+    Console.WriteLine("Fish Rising");
+}
+else if(a == b && b == c && c == d)
+{
+    Console.WriteLine("Fish At Constant Depth");
+}
+else
+{
+    Console.WriteLine("No Fish");
+}
