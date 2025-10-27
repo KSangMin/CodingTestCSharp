2a242b28b22af231f8304b43bd588f11d71da070
KSangMin <fhfpszmfnwm@naver.com>
2025-10-28 08:42:18 +0900

    [6888] Terms of Office

---
+++ _6888/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+int x = int.Parse(Console.ReadLine()!);
+int y = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+while(x <= y)
+{
+    sb.AppendLine($"All positions change in year {x}");
+    x += 60;
+}
+
+Console.Write(sb.ToString());
