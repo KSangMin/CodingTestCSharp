961ad095dd7e01e146411b34b09a45189f0dfd14
KSangMin <fhfpszmfnwm@naver.com>
2025-10-30 21:09:52 +0900

    [15372] A Simple Problem.

---
+++ _15372/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+int t = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for (int i = 0; i < t; i++)
+{
+    long n = long.Parse(Console.ReadLine()!);
+
+    long mul = n * n;
+    sb.AppendLine(mul.ToString());
+}
+
+Console.Write(sb.ToString());
