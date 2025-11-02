c93fe3f396f88677dfdeb1fa21793cbf6a1cf02f
KSangMin <fhfpszmfnwm@naver.com>
2025-11-02 23:43:31 +0900

    [2846] 오르막길

---
+++ _2846/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int[] nums = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int max = 0;
+int min = nums[0];
+int cur = nums[0];
+
+for (int i = 1; i < n; i++)
+{
+    if(nums[i] > cur)
+    {
+        max = Math.Max(max, nums[i] - min);
+    }
+    else
+    {
+        min = nums[i];
+    }
+    cur = nums[i];
+}
+
+Console.WriteLine(max);
