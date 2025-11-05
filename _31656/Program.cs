51a37ec29c3e103f9b28bbfd35ce29c386138c9e
KSangMin <fhfpszmfnwm@naver.com>
2025-11-05 21:37:48 +0900

    [31656] Sticky Keys

---
+++ _31656/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+string s = Console.ReadLine()!;
+
+StringBuilder sb = new();
+
+char before = s[0];
+sb.Append(before);
+foreach (char c in s)
+{
+    if(before != c)
+    {
+        sb.Append(c);
+    }
+    before = c;
+}
+
+Console.WriteLine(sb.ToString());
