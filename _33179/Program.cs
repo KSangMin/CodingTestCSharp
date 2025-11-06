62af6024aa280dc0c08807e6bd81d3b94b280bfb
KSangMin <fhfpszmfnwm@naver.com>
2025-11-06 22:01:32 +0900

    [33179] Hezardastan’s Annual Report

---
+++ _33179/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int[] pages = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+int ans = 0;
+foreach(int page in pages)
+{
+    ans += page / 2 + page % 2;
+}
+
+Console.WriteLine(ans);
