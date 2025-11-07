39ef0a9e51d95594366bf75fc23b41e23dd5ddb4
KSangMin <fhfpszmfnwm@naver.com>
2025-11-07 21:57:39 +0900

    [175851] This Problem's a Slam Dunk

---
+++ _17851/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] su = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+int[] us = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+Array.Sort(su);
+Array.Sort(us);
+
+int count = 0;
+for (int i = 0; i < su.Length; i++)
+{
+    if (su[i] > us[i])
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
