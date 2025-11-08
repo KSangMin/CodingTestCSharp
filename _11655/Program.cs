2b836aa572d5ead6531d94a0390545f1681737f4
KSangMin <fhfpszmfnwm@naver.com>
2025-11-09 00:45:18 +0900

    [11655] ROT13

---
+++ _11655/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+string s = Console.ReadLine()!;
+
+StringBuilder sb = new();
+
+foreach(char c in s)
+{
+    if('a' <= c && c <= 'z')
+    {
+        int ca = c + 13;
+        if(ca > 'z')
+        {
+            ca -= 26;
+        }
+        sb.Append((char)ca);
+    }
+    else if('A' <= c && c <= 'Z')
+    {
+        int ca = c + 13;
+        if (ca > 'Z')
+        {
+            ca -= 26;
+        }
+        sb.Append((char)ca);
+    }
+    else
+    {
+        sb.Append(c);
+    }
+}
+
+Console.WriteLine(sb.ToString());
