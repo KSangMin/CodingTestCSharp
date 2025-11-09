82e958dc631f034e9e4849b28b98bc1b85ce9dd8
KSangMin <fhfpszmfnwm@naver.com>
2025-11-09 19:07:42 +0900

    [17010] Time to Decompress

---
+++ _17010/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for (int i = 0; i < n; i++)
+{
+    string[] nx = Console.ReadLine()!.Split();
+    sb.AppendLine(new string(nx[1][0], int.Parse(nx[0])));
+}
+
+Console.Write(sb.ToString());
