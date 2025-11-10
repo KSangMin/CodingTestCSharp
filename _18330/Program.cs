c843ae284997734c9783b41f5699b6cfdb7bd332
KSangMin <fhfpszmfnwm@naver.com>
2025-11-11 00:22:33 +0900

    [18330] Petrol

---
+++ _18330/Program.cs
@@ -0,0 +1,6 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int k = int.Parse(Console.ReadLine()!);
+
+int next = k + 60;
+
+Console.WriteLine(n <= next ?  n * 1500 : (next * 1500 + (n - next) * 3000));
