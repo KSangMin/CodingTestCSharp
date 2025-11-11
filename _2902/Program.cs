7271250d8a51f7d4ea413d2902e65e4ba72b7728
KSangMin <fhfpszmfnwm@naver.com>
2025-11-11 20:10:55 +0900

    [2902] KMP는 왜 KMP일까?

---
+++ _2902/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+string[] s = Console.ReadLine()!.Split('-');
+
+StringBuilder sb = new();
+
+foreach (string w in s)
+{
+    sb.Append(w[0]);
+}
+
+Console.WriteLine(sb.ToString());
