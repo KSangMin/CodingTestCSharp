693cef2466d9edcd433f3a9a521c7959a6fc319e
KSangMin <fhfpszmfnwm@naver.com>
2025-11-14 20:37:00 +0900

    [32801] Generalized FizzBuzz

---
+++ _32801/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] nab = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int n = nab[0], a = nab[1], b = nab[2];
+int fizz = 0, buzz = 0, fizzbuzz = 0;
+
+for(int i = 1; i <= nab[0]; i++)
+{
+    if(i % a == 0 && i % b == 0)
+    {
+        fizzbuzz++;
+    }
+    else if(i % a == 0)
+    {
+        fizz++;
+    }
+    else if(i % b == 0)
+    {
+        buzz++;
+    }
+}
+
+Console.WriteLine($"{fizz} {buzz} {fizzbuzz}");
