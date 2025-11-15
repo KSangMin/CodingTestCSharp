5b180aea48b8900d5a3f336bbaace27fd7c82047
KSangMin <fhfpszmfnwm@naver.com>
2025-11-15 23:36:56 +0900

    [10539] 수빈이와 수열

---
+++ _10539/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+int[] nums = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int[] answer = new int[n];
+int[] accSum = new int[n];
+
+answer[0] = accSum[0] = nums[0];
+for (int i = 1; i < n; i++)
+{
+    answer[i] = nums[i] * (i + 1) - accSum[i - 1];
+    accSum[i] = accSum[i - 1] + answer[i];
+}
+
+Console.WriteLine(string.Join(" ", answer));
