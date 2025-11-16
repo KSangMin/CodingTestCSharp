a5b02fc715e150f01570e65561101beb7e7f30e2
KSangMin <fhfpszmfnwm@naver.com>
2025-11-16 20:27:47 +0900

    [20953] 고고학자 예린

---
+++ _20953/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+int t = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for(int i = 0; i < t; i++)
+{
+    int[] ab = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    long sum = ab[0] + ab[1];
+    sb.AppendLine($"{sum * sum * (sum - 1) / 2}");
+}
+
+Console.Write(sb.ToString());
