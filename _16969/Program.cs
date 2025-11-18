f79271bbae18177f47bc3740c3373a94b75fabfc
KSangMin <fhfpszmfnwm@naver.com>
2025-11-18 15:15:17 +0900

    [16969] 차량 번호판 2

---
+++ _16969/Program.cs
@@ -0,0 +1,18 @@
+﻿string s = Console.ReadLine()!;
+
+int numCount = 10, wordCount = 26;
+long ans = s[0] == 'd' ? numCount : wordCount;
+
+for(int i = 1; i < s.Length; i++)
+{
+    int count = s[i] == 'd' ? numCount : wordCount;
+    if (s[i - 1] == s[i])
+    {
+        count--;
+    }
+
+    ans *= count;
+    ans %= 1000000009;
+}
+
+Console.WriteLine(ans);
