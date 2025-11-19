94b76ff2e71f8201e4c75e041b6b2d9200ce6862
KSangMin <fhfpszmfnwm@naver.com>
2025-11-19 20:51:24 +0900

    [30402] 감마선을 맞은 컴퓨터

---
+++ _30402/Program.cs
@@ -0,0 +1,20 @@
+﻿for(int i = 0; i < 15; i++)
+{
+    char[] words = Console.ReadLine()!.ToCharArray();
+
+    if(words.Contains('w'))
+    {
+        Console.WriteLine("chunbae");
+        return;
+    }
+    else if(words.Contains('b'))
+    {
+        Console.WriteLine("nabi");
+        return;
+    }
+    else if(words.Contains('g'))
+    {
+        Console.WriteLine("yeongcheol");
+        return;
+    }
+}
