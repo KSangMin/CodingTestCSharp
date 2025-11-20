15323e1a8a990652cfdc0391db83df20183fb038
KSangMin <fhfpszmfnwm@naver.com>
2025-11-20 11:30:18 +0900

    [25600] Triathlon

---
+++ _25600/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+int max = 0;
+for(int i = 0; i < n; i++)
+{
+    int[] adg = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    int a = adg[0], d = adg[1], g = adg[2];
+
+    int sum =  a * (d + g);
+    if(a == d + g)
+    {
+        sum *= 2;
+    }
+
+    max = Math.Max(max, sum);
+}
+
+Console.WriteLine(max);
