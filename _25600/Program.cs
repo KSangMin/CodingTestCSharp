ce97eb1270491552ed41dc936dcbb9546f760354
KSangMin <fhfpszmfnwm@naver.com>
2025-11-22 01:13:09 +0900

    [31823] 악질 검거

--- _25600/Program.cs
+++ _25600/Program.cs
@@ -1,14 +1,14 @@
 ﻿int n = int.Parse(Console.ReadLine()!);
 
 int max = 0;
-for(int i = 0; i < n; i++)
+for (int i = 0; i < n; i++)
 {
     int[] adg = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
 
     int a = adg[0], d = adg[1], g = adg[2];
 
-    int sum =  a * (d + g);
-    if(a == d + g)
+    int sum = a * (d + g);
+    if (a == d + g)
     {
         sum *= 2;
     }
---
+++ _31823/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+int[] nm = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+int n = nm[0], m = nm[1];
+bool[] check = new bool[m + 1];
+
+StringBuilder sb = new();
+for(int i = 0; i < n; i++)
+{
+    string[] streak = Console.ReadLine()!.Split(' ');
+
+    int count = streak[0] == "." ? 1 : 0, max = 0;
+    for(int j = 1; j < m; j++)
+    {
+        if(streak[j] == ".")
+        {
+            count++;
+        }
+        else
+        {
+            max = Math.Max(max, count);
+            count = 0;
+        }
+    }
+    max = Math.Max(max, count);
+    check[max] = true;
+
+    sb.AppendLine($"{max} {streak[m]}");
+}
+
+StringBuilder ans = new();
+ans.AppendLine($"{check.Count(c => c == true)}");
+ans.Append(sb.ToString());
+Console.Write(ans.ToString());
