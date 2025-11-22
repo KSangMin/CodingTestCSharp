1f7bfe2b74a9bcbd068089172077e370100cd4b7
KSangMin <fhfpszmfnwm@naver.com>
2025-11-22 11:36:22 +0900

    [22277] Non-Integet Donuts

---
+++ _22277/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int[] xy = Console.ReadLine()!.Trim('$').Split('.').Select(int.Parse).ToArray();
+
+int count = 0;
+for(int i = 0; i < n; i++)
+{
+    int[] ab = Console.ReadLine()!.Trim('$').Split('.').Select(int.Parse).ToArray();
+    xy[1] += ab[1];
+    if(xy[1] >= 100)
+    {
+        xy[0] += xy[1] / 100;
+        xy[1] = xy[1] % 100;
+    }
+
+    xy[0] += ab[0];
+
+    if (xy[1] != 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
