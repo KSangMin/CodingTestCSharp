e6fe16ef351cebad1ad1603ec258be56f2510cce
KSangMin <fhfpszmfnwm@naver.com>
2025-11-24 15:01:54 +0900

    [12033] 김인천씨의 식료품가게 (Small)

---
+++ _12033/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+int t = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for(int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    List<int> prices = Console.ReadLine()!.Split(' ').Select(int.Parse).ToList();
+    List<int> ans = new();
+
+    int j = 0;
+    while(j < prices.Count)
+    {
+        if (prices[j] % 3 == 0)
+        {
+            for(int k = j + 1; k < prices.Count; k++)
+            {
+                if (prices[k] == prices[j] / 3 * 4)
+                {
+                    ans.Add(prices[j]);
+                    prices.RemoveAt(k);
+                    break;
+                }
+            }
+        }
+        j++;
+    }
+
+    sb.AppendLine($"Case #{i + 1}: " + string.Join(' ', ans));
+}
+
+Console.Write(sb.ToString());
