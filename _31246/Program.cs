5e0f6d29c5eca0f792b7b97917728307a6ccd293
KSangMin <fhfpszmfnwm@naver.com>
2025-11-26 20:31:09 +0900

    [31246] 모바일 광고 입찰

---
+++ _31246/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] nk = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+int n = nk[0], k = nk[1];
+
+List<int> diff = new();
+for (int i = 0; i < n; i++)
+{
+    int[] ab = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+    int d = ab[1] - ab[0];
+    if (d > 0)
+    {
+        diff.Add(d);
+    }
+}
+
+diff.Sort();
+
+int c = n - diff.Count;
+if (c >= k)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    Console.WriteLine(diff[k - c - 1]);
+}
