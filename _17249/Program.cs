aff2aa10ad15faef4b397cb48d9d45982f615ce8
KSangMin <fhfpszmfnwm@naver.com>
2025-11-27 22:21:13 +0900

    [17249] 태보태보 총난타

---
+++ _17249/Program.cs
@@ -0,0 +1,24 @@
+﻿string s = Console.ReadLine()!;
+
+int left = 0, right = 0;
+bool leftFlag = true;
+foreach (char c in s)
+{
+    if (c == '@')
+    {
+        if (leftFlag)
+        {
+            left++;
+        }
+        else
+        {
+            right++;
+        }
+    }
+    else if(c == '(')
+    {
+        leftFlag = false;
+    }
+}
+
+Console.WriteLine($"{left} {right}");
