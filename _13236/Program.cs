4b5d3f1e49ad7d0c2547754fc2034165bf91b2a6
KSangMin <fhfpszmfnwm@naver.com>
2025-11-28 14:13:11 +0900

    [13236] Collatz Conjecture

---
+++ _13236/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+sb.Append(n + " ");
+while (n != 1)
+{
+    n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
+
+    sb.Append(n + " ");
+}
+
+Console.WriteLine(sb.ToString());
