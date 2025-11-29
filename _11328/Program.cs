8f8a71ddc5591d9f64685ba9e347ed49038154e7
KSangMin <fhfpszmfnwm@naver.com>
2025-11-29 11:30:11 +0900

    [11328] Strfry

---
+++ _11328/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for (int i = 0; i < n; i++)
+{
+    string[] ab = Console.ReadLine()!.Split(' ');
+    string a = ab[0], b = ab[1];
+
+    string sA = string.Concat(a.OrderBy(c => c));
+    string sB = string.Concat(b.OrderBy(c => c));
+
+    sb.AppendLine(sA == sB ? "Possible" : "Impossible");
+}
+
+Console.Write(sb.ToString());
