19a2e8f7bcaa294f9ac0178585682c615a99202a
KSangMin <fhfpszmfnwm@naver.com>
2025-11-30 14:07:35 +0900

    [10799] 쇠막대기

---
+++ _10799/Program.cs
@@ -0,0 +1,31 @@
+﻿string s = Console.ReadLine()!;
+
+int count = 0;
+int ironBarCount = 0;
+
+for(int i = 0; i < s.Length; i++)
+{
+    char c = s[i];
+
+    if (c == '(')
+    {
+        if(s[i + 1] != ')')
+        {
+            count++;
+            ironBarCount++;
+        }
+    }
+    else
+    {
+        if (s[i - 1] == '(')
+        {
+            count += ironBarCount;
+        }
+        else
+        {
+            ironBarCount--;
+        }
+    }
+}
+
+Console.WriteLine(count);
