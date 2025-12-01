cbad5ceed62d47790d34a4e38ccf246a762c74ac
KSangMin <fhfpszmfnwm@naver.com>
2025-12-01 13:58:16 +0900

    [6198] 옥상 정원 꾸미기

---
+++ _6198/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+Stack<int> st = new();
+for (int i = 0; i < n; i++)
+{
+    int h = int.Parse(Console.ReadLine()!);
+
+    while (st.Count > 0 && st.Peek() < h)
+    {
+        st.Pop();
+    }
+
+    count += st.Count;
+
+    st.Push(h);
+}
+
+Console.WriteLine(count);
