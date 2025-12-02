7e634c18e101d76de4d53c07896003bdabae5e71
KSangMin <fhfpszmfnwm@naver.com>
2025-12-02 17:39:41 +0900

    [2504] 괄호의 값

---
+++ _2504/Program.cs
@@ -0,0 +1,54 @@
+﻿string s = Console.ReadLine()!;
+
+int ans = 0;
+int temp = 1;
+Stack<char> st = new();
+
+for (int i = 0; i < s.Length; i++)
+{
+    char c = s[i];
+
+
+    if (c == '(')
+    {
+        st.Push(c);
+        temp *= 2;
+    }
+    else if (c == '[')
+    {
+        st.Push(c);
+        temp *= 3;
+    }
+
+    else if (c == ')')
+    {
+        if (st.Count == 0 || st.Peek() != '(')
+        {
+            ans = 0;
+            break;
+        }
+        if (s[i - 1] == '(')
+        {
+            ans += temp;
+        }
+
+        temp /= 2;
+        st.Pop();
+    }
+    else if (c == ']')
+    {
+        if (st.Count == 0 || st.Peek() != '[')
+        {
+            ans = 0;
+            break;
+        }
+        if (s[i - 1] == '[')
+        {
+            ans += temp;
+        }
+        temp /= 3;
+        st.Pop();
+    }
+}
+
+Console.WriteLine(st.Count() > 0 ? 0 : ans);
