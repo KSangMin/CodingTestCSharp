e0d40ae7e719cfcd78b32b9f1799552c7553aa6a
KSangMin <fhfpszmfnwm@naver.com>
2025-12-03 20:58:20 +0900

    [31518] Triple Sevens

---
+++ _31518/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+bool flag = true;
+for (int i = 0; i < 3; i++)
+{
+    int[] nums = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    if (flag)
+    {
+        flag = nums.Contains(7); 
+    }
+}
+
+Console.WriteLine(flag ? 777 : 0);
