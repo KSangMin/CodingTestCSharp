5b7ad902dadc113d2a3e95b11b82c26b83772c02
KSangMin <fhfpszmfnwm@naver.com>
2025-12-05 19:19:12 +0900

    [17298] 오큰수

---
+++ _17298/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+int[] arr = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+Stack<int> st = new();
+
+for(int i = 0; i < n; i++)
+{
+    while(st.Count > 0 && arr[st.Peek()] < arr[i])
+    {
+        arr[st.Pop()] = arr[i];
+    }
+    st.Push(i);
+}
+
+while(st.Count > 0)
+{
+    arr[st.Pop()] = -1;
+}
+
+Console.WriteLine(string.Join(" ", arr));
