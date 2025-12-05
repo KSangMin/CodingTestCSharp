f41e340699b9eaed23507890db150a50e1c5d31e
KSangMin <fhfpszmfnwm@naver.com>
2025-12-05 11:54:28 +0900

    [Aoc] D1 P1

---
+++ advent_d1_1/Program.cs
@@ -0,0 +1,30 @@
+﻿int n = 4412;
+
+int pos = 50;
+int ans = 0;
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine()!;
+
+    int num = int.Parse(input.Substring(1));
+
+    if (input[0] == 'R')
+    {
+        pos = (pos + num) % 100;
+    }
+    else if (input[0] == 'L')
+    {
+        pos = (pos - num) % 100;
+        if (pos < 0)
+        {
+            pos += 100;
+        }
+    }
+
+    if(pos == 0)
+    {
+        ans++;
+    }
+}
+
+Console.WriteLine(ans);
