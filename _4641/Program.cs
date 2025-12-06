e957611df71cfc7141bc67e0cf4e22e78f88d425
KSangMin <fhfpszmfnwm@naver.com>
2025-12-06 17:44:25 +0900

    [4641] Doubles

---
+++ _4641/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+StringBuilder sb = new();
+
+while (true)
+{
+    int[] nums = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    if (nums[0] == -1)
+    {
+        break;
+    }
+
+    bool[] check = new bool[101];
+
+    for (int i = 0; i < nums.Length - 1; i++)
+    {
+        check[nums[i]] = true;
+    }
+
+    int count = 0;
+    for (int i = 0; i < nums.Length - 1; i++)
+    {
+        if (nums[i] <= 50 && check[nums[i] * 2])
+        {
+            count++;
+        }
+    }
+
+    sb.AppendLine(count.ToString());
+}
+
+Console.Write(sb.ToString());
