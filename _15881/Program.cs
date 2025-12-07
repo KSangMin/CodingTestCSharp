b482df278634f7ff39f4b02d70603cd5446ab39d
KSangMin <fhfpszmfnwm@naver.com>
2025-12-07 12:07:20 +0900

    [15881] Pen Pineapple Apple Pen

---
+++ _15881/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+string s = Console.ReadLine()!;
+
+int i = 0, count = 0;
+while(i < s.Length - 3)
+{
+    if(s.Substring(i, 4) == "pPAp")
+    {
+        i += 3;
+        count++;
+    }
+    i++;
+}
+
+Console.WriteLine(count);
