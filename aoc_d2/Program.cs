9d55c834f053758b9d69598997282ec1168529b7
KSangMin <fhfpszmfnwm@naver.com>
2025-12-07 20:52:52 +0900

    [AoC] D2 P1

---
+++ aoc_d2/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] s = Console.ReadLine()!.Split(',');
+
+long ans = 0;
+foreach (string range in s)
+{
+    long[] ab = range.Split('-').Select(long.Parse).ToArray();
+
+    for(long i = ab[0]; i <= ab[1]; i++)
+    {
+        string num = i.ToString();
+        if(num.Length % 2 == 1)
+        {
+            continue;
+        }
+
+        if(num.Substring(0, num.Length / 2)
+            == num.Substring(num.Length / 2, num.Length / 2))
+        {
+            ans += i;
+        }
+    }
+}
+
+Console.WriteLine(ans);
