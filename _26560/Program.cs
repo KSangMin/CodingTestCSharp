9ef06b5f7e1b12d72c14c313c68511f784bfe79d
KSangMin <fhfpszmfnwm@naver.com>
2025-12-10 08:46:43 +0900

    [26560] Periods

---
+++ _26560/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for(int i = 0; i < n; i++)
+{
+    string s = Console.ReadLine()!;
+
+    sb.AppendLine(s.Last() == '.' ? s : s + '.');
+}
+
+Console.Write(sb.ToString());
