06bf8fc08716414810a82565bebda6892bb57777
KSangMin <fhfpszmfnwm@naver.com>
2025-12-11 12:11:09 +0900

    [23972] 악마의 제안

---
+++ _23972/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] kn = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+long k = kn[0], n = kn[1];
+
+long ans = -1;
+
+if(n != 1)
+{
+    ans = n * k / (n - 1);
+    if(n * k % (n - 1) != 0)
+    {
+        ans++;
+    }
+}
+
+Console.WriteLine(ans);
