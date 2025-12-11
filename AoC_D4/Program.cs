e00a72263d8238e4fa4f39bc1b94cb241840bbeb
KSangMin <fhfpszmfnwm@naver.com>
2025-12-11 17:42:01 +0900

    [AoC] D4 P1

---
+++ AoC_D4/Program.cs
@@ -0,0 +1,45 @@
+﻿int n = 138;
+int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 };
+int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+string[] grid = new string[n];
+for (int i = 0; i < n; i++)
+{
+    grid[i] = Console.ReadLine()!;
+}
+
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (grid[i][j] == '@')
+        {
+            int atCount = 0;
+            for (int d = 0; d < 8; d++)
+            {
+                int nX = i + dx[d];
+                int nY = j + dy[d];
+
+                if (nX >= 0 && nX < n 
+                    && nY >= 0 && nY < n
+                    && grid[nX][nY] == '@')
+                {
+                    atCount++;
+                }
+
+                if(atCount >= 4)
+                {
+                    break;
+                }
+            }
+
+            if (atCount < 4)
+            {
+                count++;
+            }
+        }
+    }
+}
+
+Console.WriteLine(count);
