4d4a759c9739ae0fc5923175e225ddf97df92d56
KSangMin <fhfpszmfnwm@naver.com>
2025-12-13 17:25:00 +0900

    [33754] 격자 막기

---
+++ _33754/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+int[] first = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+int[] second = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+bool allOne = true;
+for (int i = 0; i < n - 1; i++)
+{
+    if (first[i] + second[i] == 0
+        || (first[i] == 0 && second[i] == 1 && first[i + 1] == 1 && second[i + 1] == 0)
+        || (first[i] == 1 && second[i] == 0 && first[i + 1] == 0 && second[i + 1] == 1))
+    {
+        Console.WriteLine(0);
+        return;
+    }
+    else if (first[i] == 0 || second[i] == 0)
+    {
+        allOne = false;
+    }
+}
+
+Console.WriteLine(allOne ? 2 : 1);
