9d8932615f24523d7dc58dd99c090f25fb532d78
KSangMin <fhfpszmfnwm@naver.com>
2025-12-15 18:38:33 +0900

    [24736] Football Scoring

---
+++ _24736/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] score = { 6, 3, 2, 1, 2 };
+
+int[] aScore = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+int[] bScore = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+int a = 0, b = 0;
+for (int i = 0; i < 5; i++)
+{
+    a += aScore[i] * score[i];
+    b += bScore[i] * score[i];
+}
+
+Console.WriteLine($"{a} {b}");
