589d8f5ce1e0e3047357f1b508e02e3e420bf986
KSangMin <fhfpszmfnwm@naver.com>
2025-12-17 11:35:00 +0900

    [1822] 차집합

---
+++ _1822/Program.cs
@@ -0,0 +1,26 @@
+﻿List<int> ab = Console.ReadLine()!.Split(' ').Select(int.Parse).ToList();
+
+List<int> a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToList();
+List<int> b = Console.ReadLine()!.Split(' ').Select(int.Parse).ToList();
+
+HashSet<int> check = new();
+foreach(int i in b)
+{
+    check.Add(i);
+}
+
+List<int> ans = new();
+foreach(int i in a)
+{
+    if (!check.Contains(i))
+    {
+        ans.Add(i);
+    }
+}
+
+Console.WriteLine(ans.Count);
+if (ans.Count > 0)
+{
+    ans.Sort();
+    Console.WriteLine(string.Join(" ", ans));
+}
