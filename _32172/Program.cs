95b327ab355505e387a66f0851bb49ffb1e78cd4
KSangMin <fhfpszmfnwm@naver.com>
2025-12-18 11:29:29 +0900

    [32172] 현권이와 신기한 수열

---
+++ _32172/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+HashSet<int> arr = new() { 0 };
+
+int before = 0;
+for (int i = 1; i <= n; i++)
+{
+    int next = before - i;
+    if (next < 0 || arr.Contains(next))
+    {
+        next = before + i;
+    }
+
+    arr.Add(next);
+    before = next;
+}
+
+Console.WriteLine(before);
