0ae875aca52a3483e63302eeb61c425a87d169d5
KSangMin <fhfpszmfnwm@naver.com>
2025-12-20 18:27:13 +0900

    [2592] 대표값

---
+++ _2592/Program.cs
@@ -0,0 +1,20 @@
+﻿int sum = 0;
+Dictionary<int, int> count = new();
+
+for(int i = 0; i < 10; i++)
+{
+    int num = int.Parse(Console.ReadLine()!);
+    sum += num;
+
+    if(count.ContainsKey(num))
+    {
+        count[num]++;
+    }
+    else
+    {
+        count[num] = 1;
+    }
+}
+
+Console.WriteLine(sum / 10);
+Console.WriteLine(count.MaxBy(kv => kv.Value).Key);
