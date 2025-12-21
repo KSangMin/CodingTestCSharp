44fdcb0a323932db32473713a9952c75518af211
KSangMin <fhfpszmfnwm@naver.com>
2025-12-21 10:26:08 +0900

    [25630] 팰린드롬 소떡소떡

---
+++ _25630/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+string s = Console.ReadLine()!;
+
+int count = 0;
+for(int i = 0; i < n / 2; i++)
+{
+    if(s[i] != s[n - 1 - i])
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
