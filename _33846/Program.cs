ebcb7e03c92b3c7572209d6780cd67b9aea25958
KSangMin <fhfpszmfnwm@naver.com>
2025-12-25 12:23:33 +0900

    [33846] 삽입 정렬을 해볼까

---
+++ _33846/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] nt = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int n = nt[0], t = nt[1];
+
+int[] arr = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+Array.Sort(arr, 0, t);
+
+Console.WriteLine(string.Join(" ", arr));
