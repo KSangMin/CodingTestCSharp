41f4ef13fc39edb7c7a158aa0d53720fe02f8dae
KSangMin <fhfpszmfnwm@naver.com>
2025-12-26 09:42:37 +0900

    [18025] Hot Hike

---
+++ _18025/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int[] arr = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int d = 1;
+int min = 41;
+for(int i = 0; i <= n - 3; i++)
+{
+    int m = Math.Max(arr[i], arr[i + 2]);
+
+    if(m < min)
+    {
+        min = m;
+        d = i + 1;
+    }
+}
+
+Console.WriteLine($"{d}\n{min}");
