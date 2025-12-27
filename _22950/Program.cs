c05bf7881fd7ee4341a04b544eb8df59738c25bb
KSangMin <fhfpszmfnwm@naver.com>
2025-12-27 13:13:49 +0900

    [22950] 이진수 나눗셈

---
+++ _22950/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+string num = Console.ReadLine()!;
+int k = int.Parse(Console.ReadLine()!);
+
+int count = Math.Min(k, n);
+
+for (int i = 0; i < count; i++)
+{
+    if(num[n - 1 - i] == '1')
+    {
+        Console.WriteLine("NO");
+        return;
+    }
+}
+
+Console.WriteLine("YES");
