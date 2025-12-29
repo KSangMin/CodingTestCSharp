261e57f6a036d135795ffd8dc82e629258134150
KSangMin <fhfpszmfnwm@naver.com>
2025-12-29 09:48:34 +0900

    [23544] Kinder Surprise

---
+++ _23544/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+HashSet<string> names = new();
+for (int i = 0; i < n; i++)
+{
+    names.Add(Console.ReadLine()!);
+}
+
+Console.WriteLine(n - names.Count);
