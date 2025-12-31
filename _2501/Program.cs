8b636fbb1cfc543004e57da463e7515a0a0845e5
KSangMin <fhfpszmfnwm@naver.com>
2025-12-31 18:00:43 +0900

    [2501] 약수 구하기

---
+++ _2501/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] nk = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+int n = nk[0], k = nk[1];
+
+int count = 0;
+for(int i = 1; i <= n; i++)
+{
+    if(n % i == 0)
+    {
+        count++;
+        if(count == k)
+        {
+            Console.WriteLine(i);
+            return;
+        }
+    }
+}
+
+Console.WriteLine(0);
