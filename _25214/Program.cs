a0d52c36100c2106002fae2473144647bdff8302
KSangMin <fhfpszmfnwm@naver.com>
2026-01-01 18:46:06 +0900

    [25214] 크림 파스타

---
+++ _25214/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+int[] num = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int min = num[0];
+int diff = 0;
+
+StringBuilder sb = new();
+
+for (int i = 0; i < n; i++)
+{
+    min = Math.Min(min, num[i]);
+    diff = Math.Max(diff, num[i] - min);
+
+    sb.Append($"{diff} ");
+}
+
+Console.WriteLine(sb.ToString().TrimEnd());
