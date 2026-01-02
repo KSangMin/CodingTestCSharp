5fc2ba6b7543016edf94dcafd05a275d020bf8b6
KSangMin <fhfpszmfnwm@naver.com>
2026-01-02 09:44:43 +0900

    [28445] 알록달록 앵무새

---
+++ _28445/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+SortedSet<string> set = new();
+for(int i = 0; i < 2; i++)
+{
+    foreach (string s in Console.ReadLine()!.Split())
+    {
+        set.Add(s);
+    }
+}
+
+StringBuilder sb = new();
+foreach (string s1 in set)
+{
+    foreach (string s2 in set)
+    {
+        sb.AppendLine($"{s1} {s2}");
+    }
+}
+
+Console.Write(sb.ToString());
