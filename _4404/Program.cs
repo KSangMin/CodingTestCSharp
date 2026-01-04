920192c9a4e7cf49f24cb026d314301575fc1efe
KSangMin <fhfpszmfnwm@naver.com>
2026-01-04 15:12:40 +0900

    [4404] Dog & Gopher

---
+++ _4404/Program.cs
@@ -0,0 +1,20 @@
+﻿double[] xy = Console.ReadLine()!.Split(' ').Select(double.Parse).ToArray();
+double gX = xy[0], gY = xy[1], dX = xy[2], dY = xy[3];
+
+string input;
+while ((input = Console.ReadLine()!) != null)
+{
+    double[] hole = input.Split(' ').Select(double.Parse).ToArray();
+    double hX = hole[0], hY = hole[1];
+
+    double gDist = (gX - hX) * (gX - hX) + (gY - hY) * (gY - hY);
+    double dDist = (dX - hX) * (dX - hX) + (dY - hY) * (dY - hY);
+
+    if (gDist * 4 <= dDist)
+    {
+        Console.WriteLine($"The gopher can escape through the hole at ({hX:F3},{hY:F3}).");
+        return;
+    }
+}
+
+Console.WriteLine("The gopher cannot escape.");
