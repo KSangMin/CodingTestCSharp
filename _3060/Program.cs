7eeb653e0e297feb871246ae2ae84137eebcef40
KSangMin <fhfpszmfnwm@naver.com>
2026-01-04 12:06:07 +0900

    [3060] 욕심쟁이 돼지

---
+++ _3060/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+int t = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    int[] num = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    int sum = num.Sum();
+    int count = 1;
+    while(sum <= n)
+    {
+        count++;
+        sum *= 4;
+    }
+
+    sb.AppendLine(count.ToString());
+}
+
+Console.Write(sb.ToString());
