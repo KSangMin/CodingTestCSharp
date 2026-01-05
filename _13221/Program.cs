52c3165df416b88d277a85dd30d520874e23a92c
KSangMin <fhfpszmfnwm@naver.com>
2026-01-05 11:31:30 +0900

    [13221] Manhattan

---
+++ _13221/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] start = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int sX = start[0], sY = start[1];
+int n = int.Parse(Console.ReadLine()!);
+
+int min = int.MaxValue, mX = 0, mY = 0;
+for (int i = 0; i < n; i++)
+{
+    int[] point = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    int pX = point[0], pY = point[1];
+    
+    int manhatanDistance = Math.Abs(sX - pX) + Math.Abs(sY - pY);
+    if (manhatanDistance < min)
+    {
+        min = manhatanDistance;
+        mX = pX;
+        mY = pY;
+    }
+}
+
+Console.WriteLine($"{mX} {mY}");
