c9a042444d57f87c4af47613cdd7972c5a0d672a
KSangMin <fhfpszmfnwm@naver.com>
2026-01-07 10:16:13 +0900

    [34925] 실크의 노래

---
+++ _34925/Program.cs
@@ -0,0 +1,19 @@
+﻿long[] hs = Console.ReadLine()!.Split().Select(long.Parse).ToArray();
+
+Console.WriteLine(Fight(hs[0], hs[1]));
+
+long Fight(long h, long s)
+{
+    long total = h;
+
+    if (h > 4)
+    {
+        total = h + s * 3;
+    }
+    else if (h > 2)
+    {
+        total = h + s * 2;
+    }
+
+    return (total + 1) / 2;
+}
