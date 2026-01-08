4a155b4feaa920fbe2d06eb7a189ab7cee26413e
KSangMin <fhfpszmfnwm@naver.com>
2026-01-08 19:15:00 +0900

    [6904] Picture Perfect

---
+++ _6904/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+int n;
+
+StringBuilder sb = new();
+while ((n = int.Parse(Console.ReadLine()!)) != 0)
+{
+    int mA = 1, mB = n;
+
+    for(int i = 1; i * i <= n; i++)
+    {
+        if(n % i == 0)
+        {
+            mA = i;
+            mB = n / i;
+        }
+    }
+
+    sb.AppendLine($"Minimum perimeter is {2 * (mA + mB)} with dimensions {mA} x {mB}");
+}
+
+Console.WriteLine(sb.ToString());
