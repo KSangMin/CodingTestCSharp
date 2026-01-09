9cd0d7a63c55714cd7564cfb0800a5e6f299a9b0
KSangMin <fhfpszmfnwm@naver.com>
2026-01-09 11:09:56 +0900

    [4807] Iterated Difference

---
+++ _4807/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+int n, order = 0;
+
+StringBuilder sb = new();
+while((n = int.Parse(Console.ReadLine()!)) != 0)
+{
+    int[] arr = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    order++;
+    int count = 0;
+    while (count <= 1000)
+    {
+        bool done = true;
+        for(int i = 0; i < n - 1; i++)
+        {
+            if (arr[i] != arr[i + 1])
+            {
+                done = false;
+                break;
+            }
+        }
+
+        if (done)
+        {
+            break;
+        }
+
+        count++;
+
+        int[] temp = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            temp[i] = Math.Abs(arr[i] - arr[(i + 1) % n]);
+        }
+        arr = temp;
+    }
+
+    sb.Append($"Case {order}: ");
+    sb.AppendLine(count > 1000 ? "not attained" : $"{count} iterations");
+}
+
+Console.WriteLine(sb.ToString());
