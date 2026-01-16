fe1dff42ff725024381ffb52b1e4e9899482dc66
KSangMin <fhfpszmfnwm@naver.com>
2026-01-16 11:48:18 +0900

    [32154] SUAPC 2024 Winter

---
+++ _32154/Program.cs
@@ -0,0 +1,18 @@
+﻿List<List<char>> scores = new()
+{
+    new(){'A','B','C','D','E','F','G','H','J','L','M' },
+    new(){'A','C','E','F','G','H','I','L','M' },
+    new(){'A','C','E','F','G','H','I','L','M' },
+    new(){'A','B','C','E','F','G','H','L','M' },
+    new(){'A','C','E','F','G','H','L','M' },
+    new(){'A','C','E','F','G','H','L','M' },
+    new(){'A','C','E','F','G','H','L','M' },
+    new(){'A','C','E','F','G','H','L','M' },
+    new(){'A','C','E','F','G','H','L','M' },
+    new(){'A','B','C','F','G','H','L','M' }
+};
+
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(scores[n - 1].Count);
+Console.WriteLine(string.Join(' ', scores[n - 1]));
