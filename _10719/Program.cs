475558e1b8c5775cb08103f49d55edcc09bc8a8a
KSangMin <fhfpszmfnwm@naver.com>
2026-01-17 12:58:05 +0900

    [10719] 쿠폰

---
+++ _10719/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for(int i = 0; i < n; i++)
+{
+    sb.AppendLine($"${double.Parse(Console.ReadLine()!) * 0.8:F2}");
+}
+
+Console.WriteLine(sb.ToString());
