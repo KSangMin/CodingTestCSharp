6496cd6e2a22187656d14a0f381599439134bd99
KSangMin <fhfpszmfnwm@naver.com>
2026-01-18 12:54:47 +0900

    [14041] Arrival Time

---
+++ _14041/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] hm = Console.ReadLine()!.Split(':').Select(int.Parse).ToArray();
+
+int m = hm[0] * 60 + hm[1];
+int total = 120;
+
+while (total > 0)
+{
+    int diff = 0;
+    if (m < 420)
+    {
+        diff = Math.Min(420 - m, total);
+    }
+    else if (m < 600)
+    {
+        diff = Math.Min(600 - m, total * 2);
+        total += diff / 2;
+    }
+    else if (m < 900)
+    {
+        diff = Math.Min(900 - m, total);
+    }
+    else if (m < 1140)
+    {
+        diff = Math.Min(1140 - m, total * 2);
+        total += diff / 2;
+    }
+    else
+    {
+        diff = total;
+    }
+
+    m = (m + diff) % 1440;
+    total -= diff;
+}
+
+Console.WriteLine($"{m / 60:D2}:{m % 60:D2}");
