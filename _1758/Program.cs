6f69bf0495d0f609a88c86ca18157e19d6daa63c
KSangMin <fhfpszmfnwm@naver.com>
2026-01-21 11:26:30 +0900

    [1758] 알바생 강호

---
+++ _1758/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+List<int> people = new();
+
+for(int i = 0; i < n; i++)
+{
+    people.Add(int.Parse(Console.ReadLine()!));
+}
+
+people.Sort();
+
+long sum = 0;
+for(int i = n - 1; i >= 0; i--)
+{
+    int tip = people[i] - (n - 1 - i);
+    if (tip <= 0)
+    {
+        break;
+    }
+    sum += Math.Max(0, tip);
+}
+
+Console.WriteLine(sum);
