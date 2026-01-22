cd39f73f04b6ef696db6577d005d90dbb016f8cd
KSangMin <fhfpszmfnwm@naver.com>
2026-01-22 10:39:31 +0900

    [2161] 카드1

---
+++ _2161/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+Queue<int> card = new();
+
+for(int i = 1; i <= n; i++)
+{
+    card.Enqueue(i);
+}
+
+List<int> ans = new();
+while(card.Count > 0)
+{
+    ans.Add(card.Dequeue());
+    if(card.Count == 0)
+    {
+        break;
+    }
+    card.Enqueue(card.Dequeue());
+}
+
+Console.WriteLine(string.Join(" ", ans));
