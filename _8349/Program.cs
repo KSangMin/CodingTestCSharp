4934732f845c09201068346dbb50c8f896774e2a
KSangMin <fhfpszmfnwm@naver.com>
2026-01-23 10:30:03 +0900

    [8349] 악수

---
+++ _8349/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+List<int> dp = new() { 0, 1, 2 };
+
+for (int i = 3; i <= n; i++)
+{
+    dp.Add((dp[i - 2] + dp[i - 1]) % 10);
+}
+
+Console.WriteLine(dp[n]);
