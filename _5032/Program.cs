970596c67b5a6c9d11cb1df6ec64aaf8ba87b6a5
KSangMin <fhfpszmfnwm@naver.com>
2026-01-27 12:23:42 +0900

    [5032] 탄산 음료

---
+++ _5032/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] efc = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int e = efc[0] + efc[1], c = efc[2];
+
+int sum = 0;
+while(e >= c)
+{
+    sum += e / c;
+    e = e / c + e % c;
+}
+
+Console.WriteLine(sum);
