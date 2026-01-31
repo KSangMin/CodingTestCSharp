18fcf955c424d601c5a2b29604b7fae32df716f0
KSangMin <fhfpszmfnwm@naver.com>
2026-01-31 11:16:37 +0900

    [12437] 새로운 달력 (Small)

---
+++ _12437/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+int t = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for (int i = 0; i < t; i++)
+{
+    int[] mdw = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    int m = mdw[0], d = mdw[1], w = mdw[2];
+
+    int row = 0;
+    int wid = 0;
+    for (int j = 0; j < m; j++)
+    {
+        row += (wid + d + w - 1) / w;
+        wid = (wid + d) % w;
+    }
+
+    sb.AppendLine($"Case #{i + 1}: {row}");
+}
+
+Console.Write(sb.ToString());
