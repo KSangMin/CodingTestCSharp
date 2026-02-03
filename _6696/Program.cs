5fdc86bb370679b5a823f63fcd0383422f081263
KSangMin <fhfpszmfnwm@naver.com>
2026-02-03 10:14:00 +0900

    [6696] Too Much Water

---
+++ _6696/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+StringBuilder sb = new();
+
+while (true)
+{
+    double[] xy = Console.ReadLine()!.Split().Select(double.Parse).ToArray();
+    if (xy[0] == 0f && xy[1] == 0f)
+    {
+        break;
+    }
+
+    double sqrtDist = xy[0] * xy[0] + xy[1] * xy[1];
+    int mul = (int)Math.Ceiling(sqrtDist * Math.PI / 100d);
+    sb.AppendLine($"The property will be flooded in hour {mul}.");
+}
+
+Console.Write(sb.ToString());
