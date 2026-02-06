864a963d44179f77c0f86f31b9ab170299fcc85a
KSangMin <fhfpszmfnwm@naver.com>
2026-02-06 09:54:21 +0900

    [16609] Inflation

---
+++ _16609/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int[] gas = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+Array.Sort(gas);
+
+double minRatio = 1f;
+for(int i = 0; i < n; i++)
+{
+    if(gas[i] > i + 1)
+    {
+        Console.WriteLine("impossible");
+        return;
+    }
+
+    minRatio = Math.Min(minRatio, (double)gas[i] / (i + 1));
+}
+
+Console.WriteLine(minRatio.ToString());
