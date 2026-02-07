99067d8d157e5ee009a1f5baef280cf4fd46c53a
KSangMin <fhfpszmfnwm@naver.com>
2026-02-07 10:15:39 +0900

    [1895] 필터

---
+++ _1895/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] rc = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int r = rc[0], c = rc[1];
+List<List<int>> grid = new();
+for(int i = 0; i < r; i++)
+{
+    grid.Add(Console.ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+}
+int t = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+for (int i = 0; i < r - 2; i++)
+{
+    for(int j = 0; j < c - 2; j++)
+    {
+        List<int> filter = new();
+        for(int x = 0; x < 3; x++)
+        {
+            for(int y = 0; y < 3; y++)
+            {
+                filter.Add(grid[i + x][j + y]);
+            }
+        }
+
+        filter.Sort();
+
+        if(filter[4] >= t)
+        {
+            count++;
+        }
+    }
+}
+
+Console.WriteLine(count);
