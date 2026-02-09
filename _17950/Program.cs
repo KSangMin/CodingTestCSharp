2daa14e67e166391b621f0c82ee7fcdd5f70d095
KSangMin <fhfpszmfnwm@naver.com>
2026-02-09 09:47:27 +0900

    [17950] 스노우볼

---
+++ _17950/Program.cs
@@ -0,0 +1,16 @@
+﻿long threshold = 1000000007;
+
+int[] hx = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int h = hx[0], x = hx[1];
+
+long sum = 0, i = 1;
+while(h > 0)
+{
+    i = (i * x) % threshold;
+    sum = (sum + int.Parse(Console.ReadLine()!) * i) % threshold;
+    
+    h--;
+}
+
+Console.WriteLine(sum);
