53d5b002d254842222932b1e31863a76c294a169
KSangMin <fhfpszmfnwm@naver.com>
2026-02-10 10:20:03 +0900

    [24860] Counting Antibodies

---
+++ _24860/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] V = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int[] L = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int[] H = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int vVar = V[0] * V[1];
+int lVar = L[0] * L[1];
+int hVar = H[0] * H[1] * H[2];
+
+Console.WriteLine((vVar + lVar) * hVar);
