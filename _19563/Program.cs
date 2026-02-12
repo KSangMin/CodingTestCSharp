e6897b6a6109931f3d730d38f87f0778ee9730e2
KSangMin <fhfpszmfnwm@naver.com>
2026-02-12 10:07:12 +0900

    [19563] 개구리 1

---
+++ _19563/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] abc = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int sum = Math.Abs(abc[0]) + Math.Abs(abc[1]), c = abc[2];
+
+Console.WriteLine(sum <= c && sum % 2 == c % 2 ? "YES" : "NO");
---
+++ _34217/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] ab = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int[] cd = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int a = ab[0], b = ab[1], c = cd[0], d = cd[1];
+int h = a + c, y = b + d;
+
+Console.WriteLine(h == y ? "Either" : h > y ? "Yongdap" : "Hanyang Univ.");
