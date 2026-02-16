e6fedf28a4f220bb68c19eb07fe4a66e13bd4327
KSangMin <fhfpszmfnwm@naver.com>
2026-02-16 18:51:21 +0900

    [3733] Shares

---
+++ _3733/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+string line;
+StringBuilder sb = new();
+
+while ((line = Console.ReadLine()) != null)
+{
+    int[] ns = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    sb.AppendLine((ns[1] / (ns[0] + 1)).ToString());
+}
+
+Console.Write(sb.ToString());
