4109c103710084715051ed00bc046d65bf571f4e
KSangMin <fhfpszmfnwm@naver.com>
2026-02-18 10:58:50 +0900

    [18409] 母音を数える (Counting Vowels)

---
+++ _18409/Program.cs
@@ -0,0 +1,12 @@
+﻿char[] key = { 'a', 'i', 'u', 'e', 'o' };
+
+int n = int.Parse(Console.ReadLine()!);
+string s = Console.ReadLine()!;
+
+int sum = 0;
+for(int i = 0; i < key.Length; i++)
+{
+    sum += s.Count(c => c == key[i]);
+}
+
+Console.WriteLine(sum);
