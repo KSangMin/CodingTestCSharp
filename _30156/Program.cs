7df6272151482886f331bef14f063bfc60b3629d
KSangMin <fhfpszmfnwm@naver.com>
2026-02-20 09:53:00 +0900

    [30156] Malvika is peculiar about color of balloons

---
+++ _30156/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+int t = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new();
+for(int i = 0; i < t; i++)
+{
+    string s = Console.ReadLine()!;
+    sb.AppendLine(Math.Min(s.Count(c => c == 'a'), s.Count(c => c == 'b')).ToString());
+}
+
+Console.Write(sb.ToString());
