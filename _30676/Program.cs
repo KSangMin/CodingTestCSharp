613a82ffb9d7a1f50765c8ef467a64bdccb7472a
KSangMin <fhfpszmfnwm@naver.com>
2026-02-28 18:48:24 +0900

    [30676] 이 별은 무슨 색일까

---
+++ _30676/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+if (n >= 620) Console.WriteLine("Red");
+else if (n >= 590) Console.WriteLine("Orange");
+else if (n >= 570) Console.WriteLine("Yellow");
+else if (n >= 495) Console.WriteLine("Green");
+else if (n >= 450) Console.WriteLine("Blue");
+else if (n >= 425) Console.WriteLine("Indigo");
+else Console.WriteLine("Violet");
