68de67e2e3b542b21e1833de675cc8eb071a06db
KSangMin <fhfpszmfnwm@naver.com>
2026-03-01 17:14:12 +0900

    [9663] N-Queen

---
+++ _9663/Program.cs
@@ -0,0 +1,37 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int[] col = new int[n + 1];
+
+int count = 0;
+FindQueen(0, col, ref count);
+Console.WriteLine(count);
+
+void FindQueen(int i, int[] col, ref int count)
+{
+    int n = col.Length - 1;
+
+    if(Promising(i, col))
+    {
+        if(i == n)
+        {
+            count++;
+        }
+        else
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                col[i + 1] = j;
+                FindQueen(i + 1, col, ref count);
+            }
+        }
+    }
+}
+
+bool Promising(int i, int[] col)
+{
+    for (int j = 1; j < i; j++)
+    {
+        if (col[i] == col[j] || Math.Abs(col[i] - col[j]) == Math.Abs(i - j))
+            return false;
+    }
+    return true;
+}
