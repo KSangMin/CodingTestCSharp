4f50118f6712b6c1ac7e60e8341ad2634ee1d089
KSangMin <fhfpszmfnwm@naver.com>
2026-03-03 12:21:55 +0900

    [9935] 문자열 폭발

---
+++ _9935/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+ExplodeString();
+
+void ExplodeString()
+{
+    string s = Console.ReadLine()!;
+    string bomb = Console.ReadLine()!;
+    int b = bomb.Length;
+    char bombLast = bomb[b - 1];
+
+    StringBuilder sb = new();
+    foreach (char c in s)
+    {
+        sb.Append(c);
+
+        if(c == bombLast && sb.Length >= b)
+        {
+            sb.Replace(bomb, "", sb.Length - b, b);
+        }
+    }
+
+    Console.WriteLine(sb.Length > 0 ? sb.ToString() : "FRULA");
+}
