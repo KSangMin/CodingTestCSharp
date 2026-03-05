144d273de2654601c7774e119115dcdb4d063435
KSangMin <fhfpszmfnwm@naver.com>
2026-03-05 18:43:59 +0900

    [6193] Hungry Cows

---
+++ _6193/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int[] dp = Enumerable.Repeat(1, n).ToArray();
+List<int> cows = new();
+string s;
+while ((s = Console.ReadLine()!) != null)
+{
+    cows.AddRange(s.Split().Select(int.Parse).ToArray());
+}
+
+for (int i = 1; i < n; i++)
+{
+    for (int j = i - 1; j >= 0; j--)
+    {
+        if (cows[i] > cows[j])
+        {
+            dp[i] = Math.Max(dp[i], dp[j] + 1);
+        }
+    }
+}
+
+Console.WriteLine(dp.Max());
