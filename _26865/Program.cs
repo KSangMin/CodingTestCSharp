9ea819e21557b87cede74fd1cd4ab2356b9065e0
KSangMin <fhfpszmfnwm@naver.com>
2026-03-11 12:22:43 +0900

    [26885] Computer Network

---
+++ _26865/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] nk = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+int n = nk[0], k = nk[1];
+int[] arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+Array.Sort(arr);
+int[] sum = new int[k];
+int ans = 0;
+for (int i = 0; i < n; i++)
+{
+    int optId = Array.IndexOf(sum, sum.Min());
+    sum[optId] += arr[i];
+    ans += sum[optId];
+}
+
+Console.WriteLine(ans);
