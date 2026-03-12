b0fec1f4e2488c306d18e40533ae7812ae0c5918
KSangMin <fhfpszmfnwm@naver.com>
2026-03-12 15:42:55 +0900

    [1043] 거짓말 - UnionFind

--- _1043/Program.cs
+++ _1043/Program.cs
@@ -2,58 +2,43 @@
 int n = nm[0], m = nm[1];
 
 int[] trues = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+if (trues[0] == 0)
+{
+    Console.WriteLine(m);
+    return;
+}
 trues = trues[1..];
 
+int[] parent = new int[n + 1];
+for (int i = 1; i <= n; i++)
+{
+    parent[i] = i;
+}
+
+foreach(int t in trues)
+{
+    UnionRoot(trues[0], t);
+}
+
 List<int>[] parties = new List<int>[m];
-bool[,] graph = new bool[n + 1, n + 1];
-for(int i = 0; i < m; i++)
+for (int i = 0; i < m; i++)
 {
     parties[i] = Console.ReadLine()!.Split(' ').Select(int.Parse).ToList();
     parties[i].RemoveAt(0);
-    for(int j = 0; j < parties[i].Count; j++)
+
+    for(int j = 1; j < parties[i].Count; j++)
     {
-        for(int k = j + 1; k < parties[i].Count; k++)
-        {
-            graph[parties[i][j], parties[i][k]] = true;
-            graph[parties[i][k], parties[i][j]] = true;
-        }
-    }
-}
-
-bool[] knowing = new bool[n + 1];
-bool[] check = new bool[n + 1];
-Queue<int> q = new();
-foreach(int t in trues)
-{
-    q.Enqueue(t);
-}
-
-while(q.Count > 0)
-{
-    int cur = q.Dequeue();
-    if (check[cur])
-    {
-        continue;
-    }
-
-    check[cur] = true;
-    knowing[cur] = true;
-
-    for(int i = 1; i <= n; i++)
-    {
-        if (graph[cur, i])
-        {
-            q.Enqueue(i);
-        }
+        UnionRoot(parties[i][0], parties[i][j]);
     }
 }
 
 int ans = m;
-foreach (List<int> party in parties)
+int trueRoot = FindRoot(trues[0]);
+foreach(List<int> party in parties)
 {
     foreach(int person in party)
     {
-        if (knowing[person])
+        if(FindRoot(person) == trueRoot)
         {
             ans--;
             break;
@@ -62,3 +47,27 @@ foreach (List<int> party in parties)
 }
 
 Console.WriteLine(ans);
+
+int FindRoot(int a)
+{
+    if (parent[a] == a)
+    {
+        return a;
+    }
+    return parent[a] = FindRoot(parent[a]);
+}
+
+void UnionRoot(int a, int b)
+{
+    int rootA = FindRoot(a);
+    int rootB = FindRoot(b);
+
+    if (rootA < rootB)
+    {
+        parent[rootB] = rootA;
+    }
+    else
+    {
+        parent[rootA] = rootB;
+    }
+}
