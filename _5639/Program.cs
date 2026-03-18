9b7f15817f0961f8c774963b45a4567a7797be91
KSangMin <fhfpszmfnwm@naver.com>
2026-03-18 13:07:39 +0900

    [5693] 이진 검색 트리

---
+++ _5639/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+List<int> pre = new();
+
+using (StreamReader sr = new(Console.OpenStandardInput()))
+{
+    while (!sr.EndOfStream)
+    {
+        pre.Add(int.Parse(sr.ReadLine()!));
+    }
+}
+
+StringBuilder sb = new();
+MakePost(sb, pre, 0, pre.Count);
+
+Console.Write(sb.ToString());
+
+void MakePost(StringBuilder sb, List<int> pre, int start, int end)
+{
+    if(start >= end)
+    {
+        return;
+    }
+
+    int root = pre[start];
+    int i = start + 1;
+    while (i < end && pre[i] < root)
+    {
+        i++;
+    }
+
+    MakePost(sb, pre, start + 1, i);
+    MakePost(sb, pre, i, end);
+    sb.AppendLine($"{root}");
+}
