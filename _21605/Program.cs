23eb78924dcb74f879ccc78adbf0566e600de1cf
KSangMin <fhfpszmfnwm@naver.com>
2026-03-21 10:28:21 +0900

    [21605] 아름다운 수열

---
+++ _21605/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int len = 2 * n;
+int[] arr = new int[len];
+
+for (int i = 0; i < len - 2; i += 2)
+{
+    arr[i] = 1;
+    arr[i + 1] = -1;
+}
+
+arr[len - 2] = -1;
+arr[len - 1] = 1;
+
+Console.WriteLine(string.Join(' ', arr));
