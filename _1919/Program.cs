d173917fc63a81d864311b953419fa1fecf9ca88
KSangMin <fhfpszmfnwm@naver.com>
2026-03-27 18:52:10 +0900

    [1919] 애너그램 만들기

---
+++ _1919/Program.cs
@@ -0,0 +1,26 @@
+﻿string a = Console.ReadLine()!;
+string b = Console.ReadLine()!;
+int[] aCount = new int[26];
+int[] bCount = new int[26];
+
+foreach (char c in a)
+{
+    aCount[c - 'a']++;
+}
+
+foreach (char c in b)
+{
+    bCount[c - 'a']++;
+}
+
+int count = 0;
+for(int i = 0; i < aCount.Length; i++)
+{
+    int diff = Math.Abs(aCount[i] - bCount[i]);
+    if(diff > 0)
+    {
+        count += diff;
+    }
+}
+
+Console.WriteLine(count);
