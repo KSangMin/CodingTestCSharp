dd9fea2095fb012327fb48f450ea1a318f642f3c
KSangMin <fhfpszmfnwm@naver.com>
2026-03-27 18:52:26 +0900

    [29757] 트리 긋기

---
+++ _29757/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+
+List<(int x, int y, int id)> points = new();
+for (int i = 0; i < n; i++)
+{
+    int[] xy = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+    points.Add((xy[0], xy[1], i + 1));
+}
+
+points = points.OrderBy(p => p.x).ThenBy(p => p.y).ToList();
+
+StringBuilder sb = new();
+for (int i = 0; i < n - 1; i++)
+{
+    sb.AppendLine($"{points[i].id} {points[i + 1].id}");
+}
+
+Console.Write(sb.ToString());
