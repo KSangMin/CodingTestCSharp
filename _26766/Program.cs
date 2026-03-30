24c9183b88edad5425c22dbf4ef6d58d5455c7de
KSangMin <fhfpszmfnwm@naver.com>
2026-03-30 22:42:39 +0900

    [26766] Serca

---
+++ _26766/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+StringBuilder sb = new();
+for (int i = 0; i < n; i++)
+{
+    sb.AppendLine(@" @@@   @@@ 
+@   @ @   @
+@    @    @
+@         @
+ @       @ 
+  @     @  
+   @   @   
+    @ @    
+     @     ");
+}
+
+Console.Write(sb.ToString());
