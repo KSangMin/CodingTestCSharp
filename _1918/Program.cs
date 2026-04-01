03316913817bf7a15ce94ffacca4c196bbd0a988
KSangMin <fhfpszmfnwm@naver.com>
2026-04-01 10:50:26 +0900

    [1918] 후위 표기식

---
+++ _1918/Program.cs
@@ -0,0 +1,68 @@
+﻿string pre = Console.ReadLine()!;
+
+List<char> post = new();
+Stack<char> st = new();
+foreach (char c in pre)
+{
+    switch (c)
+    {
+        case '*':
+        case '/':
+            while (st.Count > 0)
+            {
+                if (st.Peek() == '*' || st.Peek() == '/')
+                {
+                    post.Add(st.Pop());
+                }
+                else
+                {
+                    break;
+                }
+            }
+            st.Push(c);
+            break;
+        case '+':
+        case '-':
+            while (st.Count > 0)
+            {
+                if (st.Peek() == '*' || st.Peek() == '/'
+                    || st.Peek() == '+' || st.Peek() == '-')
+                {
+                    post.Add(st.Pop());
+                }
+                else
+                {
+                    break;
+                }
+            }
+            st.Push(c);
+            break;
+        case '(':
+            st.Push(c);
+            break;
+        case ')':
+            while (st.Count > 0)
+            {
+                if (st.Peek() == '(')
+                {
+                    st.Pop();
+                    break;
+                }
+                else
+                {
+                    post.Add(st.Pop());
+                }
+            }
+            break;
+        default:
+            post.Add(c);
+            break;
+    }
+}
+
+while(st.Count > 0)
+{
+    post.Add(st.Pop());
+}
+
+Console.WriteLine(string.Join("", post));
