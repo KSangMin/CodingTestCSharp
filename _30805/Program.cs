dc04d14a577c115cf110d24eb38f0f973e9a6d26
KSangMin <fhfpszmfnwm@naver.com>
2026-04-04 12:47:05 +0900

    [30805] 사전 순 최대 공통 부분 순열

---
+++ _30805/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int[] a = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int m = int.Parse(Console.ReadLine()!);
+int[] b = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+List<int> result = new();
+int aIndex = 0, bIndex = 0;
+
+while (true)
+{
+    List<int> common = a.Skip(aIndex).Intersect(b.Skip(bIndex)).ToList();
+    if(common.Count <= 0)
+    {
+        break;
+    }
+
+    int max = common.Max();
+
+    int posA = Array.IndexOf(a, max, aIndex);
+    int posB = Array.IndexOf(b, max, bIndex);
+
+    result.Add(max);
+    aIndex = posA + 1;
+    bIndex = posB + 1;
+}
+
+Console.WriteLine(result.Count);
+if (result.Count > 0)
+{
+    Console.WriteLine(string.Join(" ", result));
+}
