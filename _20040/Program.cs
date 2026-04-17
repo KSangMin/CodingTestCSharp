69352e1d25a9ac5fee656ea200c19b34fafb9aeb
KSangMin <fhfpszmfnwm@naver.com>
2026-04-17 09:05:38 +0900

    [20040] 사이클 게임

---
+++ _20040/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] nm = Console.ReadLine ()!.Split().Select(int.Parse).ToArray();
+int n = nm[0], m = nm[1];
+
+int[] parent = new int[n];
+for (int i = 0; i < n; i++)
+{
+    parent[i] = i;
+}
+
+for (int i = 0; i < m; i++ )
+{
+    int[] sd = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    int sRoot = FindRoot(parent, sd[0]);
+    int eRoot = FindRoot(parent, sd[1]);
+
+    if (sRoot == eRoot)
+    {
+        Console.WriteLine(i + 1);
+        return;
+    }
+
+    parent[eRoot] = sRoot;
+}
+
+Console.WriteLine(0);
+
+int FindRoot(int[] parent, int node)
+{
+    return parent[node] == node ? node : parent[node] = FindRoot(parent, parent[node]);
+}
