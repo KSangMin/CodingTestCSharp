8ff5845e109b7485b135e9683f5bab56e870679e
KSangMin <fhfpszmfnwm@naver.com>
2026-04-19 13:33:04 +0900

    [27172] 수 나누기 게임

---
+++ _27172/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+const int MAX = 1000000;
+
+int n = int.Parse(Console.ReadLine()!);
+int[] player = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+bool[] exist = new bool[MAX + 1];
+foreach(int p in player)
+{
+    exist[p] = true;
+}
+
+int[] score = new int[MAX + 1];
+foreach(int p in player)
+{
+    for(int i = p * 2; i <= MAX; i += p)
+    {
+        if (exist[i])
+        {
+            score[i]--;
+            score[p]++;
+        }
+    }
+}
+
+StringBuilder sb = new();
+foreach(int p in player)
+{
+    sb.Append(score[p]).Append(' ');
+}
+Console.WriteLine(sb.ToString());
