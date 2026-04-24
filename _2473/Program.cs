fb81050e626f4a396e27bfe1705f4f92281b543a
KSangMin <fhfpszmfnwm@naver.com>
2026-04-24 10:48:50 +0900

    [2473] 세 용액

---
+++ _2473/Program.cs
@@ -0,0 +1,42 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+long[] arr = Console.ReadLine()!.Split(' ').Select(long.Parse).ToArray();
+Array.Sort(arr);
+
+long min = long.MaxValue;
+long[] result = new long[3];
+
+for (int i = 0; i < n - 2; i++)
+{
+    int left = i + 1;
+    int right = n - 1;
+
+    while (left < right)
+    {
+        long sum = arr[i] + arr[left] + arr[right];
+
+        long abs = Math.Abs(sum);
+        if (abs < min)
+        {
+            min = abs;
+            result[0] = arr[i];
+            result[1] = arr[left];
+            result[2] = arr[right];
+        }
+
+        if (sum < 0)
+        {
+            left++;
+        }
+        else if (sum > 0)
+        {
+            right--;
+        }
+        else
+        {
+            Console.WriteLine($"{result[0]} {result[1]} {result[2]}");
+            return;
+        }
+    }
+}
+
+Console.WriteLine($"{result[0]} {result[1]} {result[2]}");
